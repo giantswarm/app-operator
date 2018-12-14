37de62499ebc8ca08a18d45a5f9035650725f612
jihyuk <jhbok@giantswarm.io>
2018-12-14 14:24:53 +0100

    address review 2

--- service/service.go
+++ service/service.go
@@ -3,16 +3,11 @@ package service
 import (
 	"sync"
 
+	"github.com/giantswarm/app-operator/flag"
 	"github.com/giantswarm/microendpoint/service/version"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
-	"github.com/giantswarm/operatorkit/client/k8srestconfig"
 	"github.com/spf13/viper"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-
-	"github.com/giantswarm/app-operator/flag"
-	"github.com/giantswarm/app-operator/service/healthz"
 )
 
 // Config represents the configuration used to create a new service.
@@ -29,7 +24,6 @@ type Config struct {
 
 // Service is a type providing implementation of microkit service interface.
 type Service struct {
-	Healthz *healthz.Service
 	Version *version.Service
 
 	// Internals
@@ -47,43 +41,11 @@ func New(config Config) (*Service, error) {
 	if config.Viper == nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.Viper must not be empty", config)
 	}
+
 	var err error
-	var restConfig *rest.Config
-	{
-		c := k8srestconfig.Config{
-			Logger: config.Logger,
 
-			Address:   config.Viper.GetString(config.Flag.Service.Kubernetes.Address),
-			InCluster: config.Viper.GetBool(config.Flag.Service.Kubernetes.InCluster),
-			TLS: k8srestconfig.TLSClientConfig{
-				CAFile:  config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CAFile),
-				CrtFile: config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CrtFile),
-				KeyFile: config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.KeyFile),
-			},
-		}
-
-		restConfig, err = k8srestconfig.New(c)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-	}
-
-	k8sClient, err := kubernetes.NewForConfig(restConfig)
 	if err != nil {
 		return nil, microerror.Mask(err)
-	}
-
-	var healthzService *healthz.Service
-	{
-		c := healthz.Config{
-			K8sClient: k8sClient,
-			Logger:    config.Logger,
-		}
-
-		healthzService, err = healthz.New(c)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
 	}
 
 	var versionService *version.Service
@@ -103,7 +65,6 @@ func New(config Config) (*Service, error) {
 	}
 
 	newService := &Service{
-		Healthz: healthzService,
 		Version: versionService,
 
 		// Internals
