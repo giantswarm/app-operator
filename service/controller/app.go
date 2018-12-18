6939d79ea96ec62fd3427b17e77098abd3456816
jihyuk <jhbok@giantswarm.io>
2018-12-18 18:06:29 +0100

    wip: add controller

---
+++ service/controller/app.go
@@ -0,0 +1,108 @@
+package controller
+
+import (
+	"github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1"
+	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"github.com/giantswarm/apprclient"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/client/k8scrdclient"
+	"github.com/giantswarm/operatorkit/controller"
+	"github.com/giantswarm/operatorkit/informer"
+	"github.com/spf13/afero"
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/client-go/kubernetes"
+	//"github.com/giantswarm/app-operator/service/controller/v1"
+)
+
+const AppControllerSuffix = "-app"
+
+type AppConfig struct {
+	ApprClient   apprclient.Interface
+	G8sClient    versioned.Interface
+	K8sClient    kubernetes.Interface
+	K8sExtClient apiextensionsclient.Interface
+	Logger       micrologger.Logger
+
+	ProjectName    string
+	WatchNamespace string
+}
+
+type App struct {
+	*controller.Controller
+}
+
+func NewApp(config AppConfig) (*App, error) {
+	var err error
+
+	if config.G8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.G8sClient must not be empty")
+	}
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.K8sClient must not be empty")
+	}
+	if config.K8sExtClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.K8sExtClient must not be empty")
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Logger must not be empty")
+	}
+
+	if config.ProjectName == "" {
+		return nil, microerror.Maskf(invalidConfigError, "config.ProjectName must not be empty")
+	}
+
+	var crdClient *k8scrdclient.CRDClient
+	{
+		c := k8scrdclient.Config{
+			K8sExtClient: config.K8sExtClient,
+			Logger:       config.Logger,
+		}
+
+		crdClient, err = k8scrdclient.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var newInformer *informer.Informer
+	{
+		c := informer.Config{
+			Logger:  config.Logger,
+			Watcher: config.G8sClient.CoreV1alpha1().AppConfigs(config.WatchNamespace),
+
+			RateWait:     informer.DefaultRateWait,
+			ResyncPeriod: informer.DefaultResyncPeriod,
+		}
+
+		newInformer, err = informer.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var appController *controller.Controller
+	{
+		c := controller.Config{
+			CRD:          v1alpha1.NewAppCRD(),
+			CRDClient:    crdClient,
+			Informer:     newInformer,
+			Logger:       config.Logger,
+			ResourceSets: []*controller.ResourceSet{},
+			RESTClient:   config.G8sClient.CoreV1alpha1().RESTClient(),
+
+			Name: config.ProjectName + appControllerSuffix,
+		}
+
+		appController, err = controller.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	c := &App{
+		Controller: appController,
+	}
+
+	return c, nil
+}
