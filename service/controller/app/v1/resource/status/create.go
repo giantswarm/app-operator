8a82fa911c92bfe0c4fb3ed7763277ee0b547be1
jihyuk <jhbok@giantswarm.io>
2019-01-30 10:12:41 +0100

    address review #2

--- service/controller/app/v1/resource/status/create.go
+++ service/controller/app/v1/resource/status/create.go
@@ -23,28 +23,29 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		return microerror.Mask(err)
 	}
 
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("getting status for chart %#q", name))
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("finding status for chart %#q", name))
 	chart, err := g8sClient.ApplicationV1alpha1().Charts(r.watchNamespace).Get(name, metav1.GetOptions{})
 	if err != nil {
 		return microerror.Maskf(notFoundError, "chart %#q in namespace %#q", name, r.watchNamespace)
 	}
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found status for chart %#q", name))
 
 	if chart.Status.Status != "" && key.AppStatus(cr) != chart.Status.Status {
 		r.logger.LogCtx(ctx, "level", "debug", "message",
 			fmt.Sprintf("setting app %#q status as %#q", name, chart.Status.Status))
 
 		customResourceCopy := cr.DeepCopy()
+		customResourceCopy.Status.AppVersion = chart.Status.AppVersion
 		customResourceCopy.Status.LastDeployed = *chart.Status.LastDeployed.DeepCopy()
 		customResourceCopy.Status.Status = chart.Status.Status
 		customResourceCopy.Status.Version = chart.Status.Version
-		customResourceCopy.Status.AppVersion = chart.Status.AppVersion
 
 		_, err = r.g8sClient.ApplicationV1alpha1().Apps(cr.Namespace).UpdateStatus(customResourceCopy)
 		if err != nil {
 			return microerror.Mask(err)
 		}
 
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("status set for chart %#q", name))
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("status set for app %#q", name))
 	} else {
 		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("status for chart %#q already set to %#q", name, chart.Status.Status))
 	}
