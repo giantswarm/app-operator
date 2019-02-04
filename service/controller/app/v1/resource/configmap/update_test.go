0ba7bc6a9ffd535158751f817b0aefb9af3d4f2b
Ross Fairbanks <ross@giantswarm.io>
2019-02-04 10:43:09 +0000

    Use go-cmp for tests

--- service/controller/app/v1/resource/configmap/update_test.go
+++ service/controller/app/v1/resource/configmap/update_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1"
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned/fake"
 	"github.com/giantswarm/micrologger/microloggertest"
+	"github.com/google/go-cmp/cmp"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clientgofake "k8s.io/client-go/kubernetes/fake"
@@ -155,7 +156,7 @@ func Test_Resource_newUpdateChange(t *testing.T) {
 			}
 
 			if !reflect.DeepEqual(configMap, tc.expectedConfigMap) {
-				t.Fatalf("configMap == %q, want %q", configMap, tc.expectedConfigMap)
+				t.Fatalf("want matching configmap \n %s", cmp.Diff(configMap, tc.expectedConfigMap))
 			}
 		})
 	}
