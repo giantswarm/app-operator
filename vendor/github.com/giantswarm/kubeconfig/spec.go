7a5780ec1bab36c0fd60bf0fa40bdf5143138c49
jihyuk <jhbok@giantswarm.io>
2019-02-06 17:09:38 +0100

    update vendor

--- vendor/github.com/giantswarm/kubeconfig/spec.go
+++ vendor/github.com/giantswarm/kubeconfig/spec.go
@@ -6,6 +6,7 @@ import (
 	"github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1"
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 type Interface interface {
@@ -18,4 +19,8 @@ type Interface interface {
 	// in the kubeconfig section of the app CR. If this is empty a clientset for
 	// the current cluster is returned.
 	NewK8sClientForApp(ctx context.Context, app v1alpha1.App) (kubernetes.Interface, error)
+
+	// NewRESTConfigForApp returns a Kubernetes REST Config for the cluster configured
+	// in the kubeconfig section of the app CR.
+	NewRESTConfigForApp(ctx context.Context, app v1alpha1.App) (*rest.Config, error)
 }
