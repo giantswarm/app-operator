01bd8504893f94fb0fb6f09addef148c275afff8
Ross Fairbanks <ross@giantswarm.io>
2019-02-14 16:30:04 +0100

    vendor: updated kubeconfig

--- vendor/github.com/giantswarm/kubeconfig/kubeconfig.go
+++ vendor/github.com/giantswarm/kubeconfig/kubeconfig.go
@@ -10,6 +10,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -53,27 +54,20 @@ func New(config Config) (*KubeConfig, error) {
 // NewG8sClientForApp returns a generated clientset for the cluster configured
 // in the kubeconfig section of the app CR. If this is empty a clientset for
 // the current cluster is returned.
-func (k KubeConfig) NewG8sClientForApp(ctx context.Context, app v1alpha1.App) (versioned.Interface, error) {
-	secretName := secretName(app)
-
+func (k *KubeConfig) NewG8sClientForApp(ctx context.Context, app v1alpha1.App) (versioned.Interface, error) {
 	// KubeConfig is not configured so connect to current cluster.
-	if secretName == "" {
+	if secretName(app) != "" {
 		return k.g8sClient, nil
 	}
 
-	kubeConfig, err := k.getKubeConfigFromSecret(ctx, secretName, secretNamespace(app))
+	restConfig, err := k.NewRESTConfigForApp(ctx, app)
 	if err != nil {
-		return nil, err
-	}
-
-	restConfig, err := clientcmd.RESTConfigFromKubeConfig(kubeConfig)
-	if err != nil {
-		return nil, err
+		return nil, microerror.Mask(err)
 	}
 
 	client, err := versioned.NewForConfig(restConfig)
 	if err != nil {
-		return nil, err
+		return nil, microerror.Mask(err)
 	}
 	return client, nil
 }
@@ -81,44 +75,53 @@ func (k KubeConfig) NewG8sClientForApp(ctx context.Context, app v1alpha1.App) (v
 // NewK8sClientForApp returns a Kubernetes clientset for the cluster configured
 // in the kubeconfig section of the app CR. If this is empty a clientset for
 // the current cluster is returned.
-func (k KubeConfig) NewK8sClientForApp(ctx context.Context, app v1alpha1.App) (kubernetes.Interface, error) {
-	secretName := secretName(app)
-
+func (k *KubeConfig) NewK8sClientForApp(ctx context.Context, app v1alpha1.App) (kubernetes.Interface, error) {
 	// KubeConfig is not configured so connect to current cluster.
-	if secretName == "" {
+	if secretName(app) == "" {
 		return k.k8sClient, nil
 	}
 
-	kubeConfig, err := k.getKubeConfigFromSecret(ctx, secretName, secretNamespace(app))
+	restConfig, err := k.NewRESTConfigForApp(ctx, app)
 	if err != nil {
-		return nil, err
-	}
-
-	restConfig, err := clientcmd.RESTConfigFromKubeConfig(kubeConfig)
-	if err != nil {
-		return nil, err
+		return nil, microerror.Mask(err)
 	}
 
 	client, err := kubernetes.NewForConfig(restConfig)
 	if err != nil {
-		return nil, err
+		return nil, microerror.Mask(err)
 	}
 	return client, nil
 }
 
+func (k *KubeConfig) NewRESTConfigForApp(ctx context.Context, app v1alpha1.App) (*rest.Config, error) {
+	secretName := secretName(app)
+	secretNamespace := secretNamespace(app)
+
+	kubeConfig, err := k.getKubeConfigFromSecret(ctx, secretName, secretNamespace)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	restConfig, err := clientcmd.RESTConfigFromKubeConfig(kubeConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	return restConfig, nil
+}
+
 // getKubeConfigFromSecret returns KubeConfig bytes based on the specified secret information.
-func (k KubeConfig) getKubeConfigFromSecret(ctx context.Context, secretName, secretNamespace string) ([]byte, error) {
+func (k *KubeConfig) getKubeConfigFromSecret(ctx context.Context, secretName, secretNamespace string) ([]byte, error) {
 	secret, err := k.k8sClient.CoreV1().Secrets(secretNamespace).Get(secretName, metav1.GetOptions{})
 	if errors.IsNotFound(err) {
-		return nil, err
+		return nil, microerror.Maskf(notFoundError, "Secret %#q in Namespace %#q", secretName, secretNamespace)
 	} else if _, isStatus := err.(*errors.StatusError); isStatus {
-		return nil, err
+		return nil, microerror.Mask(err)
 	} else if err != nil {
-		return nil, err
+		return nil, microerror.Mask(err)
 	}
 	if bytes, ok := secret.Data["kubeConfig"]; ok {
 		return bytes, nil
 	} else {
-		return nil, notFoundError
+		return nil, microerror.Maskf(notFoundError, "Secret %#q in Namespace %#q does not have kubeConfig key in its data", secretName, secretNamespace)
 	}
 }
