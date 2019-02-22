08abef169cdf42767bda1fe7c4c9b45857ef14a6
jihyuk <jhbok@giantswarm.io>
2019-02-22 09:07:27 +0100

    address review #4

--- flag/service/service.go
+++ flag/service/service.go
@@ -1,16 +1,12 @@
 package service
 
 import (
+	"github.com/giantswarm/app-operator/flag/service/appcatalog"
 	"github.com/giantswarm/app-operator/flag/service/kubernetes"
 )
 
-// AppCatalog is a data structure to hold AppCatalog specific command line configuration flags.
-type AppCatalog struct {
-	IndexNamespace string
-}
-
 // Service is an intermediate data structure for command line configuration flags.
 type Service struct {
-	AppCatalog AppCatalog
+	AppCatalog appcatalog.AppCatalog
 	Kubernetes kubernetes.Kubernetes
 }
