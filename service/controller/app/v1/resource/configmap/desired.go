873332fe8eae29ed07d3ab3ba2167caca92e835a
Ross Fairbanks <ross@giantswarm.io>
2019-03-05 14:33:27 +0100

    Set desired state for configmap

---
+++ service/controller/app/v1/resource/configmap/desired.go
@@ -0,0 +1,103 @@
+package configmap
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1"
+	"github.com/giantswarm/microerror"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/giantswarm/app-operator/pkg/label"
+	"github.com/giantswarm/app-operator/service/controller/app/v1/controllercontext"
+	"github.com/giantswarm/app-operator/service/controller/app/v1/key"
+	appcatalogkey "github.com/giantswarm/app-operator/service/controller/appcatalog/v1/key"
+)
+
+func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interface{}, error) {
+	cr, err := key.ToCustomResource(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	data, err := r.mergeConfigMapData(ctx, cr)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	if data == nil || len(data) == 0 {
+		// No data so return early.
+		return nil, nil
+	}
+
+	configMap := &corev1.ConfigMap{
+		Data: data,
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      key.ConfigMapName(cr),
+			Namespace: cr.ObjectMeta.Namespace,
+			Labels: map[string]string{
+				label.ManagedBy: r.projectName,
+			},
+		},
+	}
+
+	return configMap, nil
+}
+
+func (r *Resource) getConfigMap(ctx context.Context, configMapName, configMapNamespace string) (*corev1.ConfigMap, error) {
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("looking for configmap %#q in namespace %#q", configMapName, configMapNamespace))
+
+	configMap, err := r.k8sClient.CoreV1().ConfigMaps(configMapNamespace).Get(configMapName, metav1.GetOptions{})
+	if apierrors.IsNotFound(err) {
+		return nil, microerror.Maskf(notFoundError, "config map %#q in namespace %#q not found", configMapName, configMapNamespace)
+	} else if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found configmap %#q in namespace %#q", configMapName, configMapNamespace))
+
+	return configMap, nil
+}
+
+func (r *Resource) mergeConfigMapData(ctx context.Context, cr v1alpha1.App) (map[string]string, error) {
+	data := make(map[string]string)
+
+	cc, err := controllercontext.FromContext(ctx)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	appConfigMapName := key.ConfigMapName(cr)
+	catalogConfigMapName := appcatalogkey.ConfigMapName(cc.AppCatalog)
+
+	if appConfigMapName == "" && catalogConfigMapName == "" {
+		// Return early as there is no config.
+		return nil, nil
+	}
+
+	if appConfigMapName != "" && catalogConfigMapName != "" {
+		return nil, microerror.Maskf(failedExecutionError, "merging app and catalog configmaps is not yet supported")
+	}
+
+	if appConfigMapName != "" && catalogConfigMapName == "" {
+		appConfigMap, err := r.getConfigMap(ctx, appConfigMapName, key.ConfigMapNamespace(cr))
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+
+		data = appConfigMap.Data
+	}
+
+	if appConfigMapName == "" && catalogConfigMapName != "" {
+		catalogConfigMap, err := r.getConfigMap(ctx, catalogConfigMapName, appcatalogkey.ConfigMapNamespace(cc.AppCatalog))
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+
+		data = catalogConfigMap.Data
+	}
+
+	return data, nil
+}
--- service/controller/app/v1/resource/configmap/error.go
+++ service/controller/app/v1/resource/configmap/error.go
@@ -2,6 +2,15 @@ package configmap
 
 import "github.com/giantswarm/microerror"
 
+var failedExecutionError = &microerror.Error{
+	Kind: "failedExecution",
+}
+
+// IsFailedExecution asserts failedExecution
+func IsFailedExecution(err error) bool {
+	return microerror.Cause(err) == failedExecutionError
+}
+
 var invalidConfigError = &microerror.Error{
 	Kind: "invalidConfigError",
 }
@@ -9,6 +18,15 @@ var invalidConfigError = &microerror.Error{
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
+}
+
+var notFoundError = &microerror.Error{
+	Kind: "notFoundError",
+}
+
+// IsNotFound asserts notFoundError.
+func IsNotFound(err error) bool {
+	return microerror.Cause(err) == notFoundError
 }
 
 var wrongTypeError = &microerror.Error{
