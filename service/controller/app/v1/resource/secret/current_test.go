5d2f638391cfec80d84954a1d50f3110b0bdd84f
Ross Fairbanks <ross@giantswarm.io>
2019-03-07 16:13:35 +0100

    Add current and desired state

---
+++ service/controller/app/v1/resource/secret/current_test.go
@@ -0,0 +1,180 @@
+package secret
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1"
+	"github.com/giantswarm/apiextensions/pkg/clientset/versioned/fake"
+	"github.com/giantswarm/micrologger/microloggertest"
+	"github.com/google/go-cmp/cmp"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgofake "k8s.io/client-go/kubernetes/fake"
+
+	"github.com/giantswarm/app-operator/service/controller/app/v1/controllercontext"
+)
+
+func Test_Resource_GetCurrentState(t *testing.T) {
+	tests := []struct {
+		name           string
+		obj            *v1alpha1.App
+		secret         *corev1.Secret
+		expectedSecret *corev1.Secret
+		errorMatcher   func(error) bool
+	}{
+		{
+			name: "case 0: basic match",
+			obj: &v1alpha1.App{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-app",
+					Namespace: "giantswarm",
+				},
+				Spec: v1alpha1.AppSpec{
+					Config: v1alpha1.AppSpecConfig{
+						Secret: v1alpha1.AppSpecConfigSecret{
+							Name:      "app-secrets",
+							Namespace: "default",
+						},
+					},
+					Namespace: "kube-system",
+				},
+			},
+			secret: &corev1.Secret{
+				StringData: map[string]string{
+					"key": "value",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-app-chart-secrets",
+					Namespace: "kube-system",
+				},
+			},
+			expectedSecret: &corev1.Secret{
+				StringData: map[string]string{
+					"key": "value",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-app-chart-secrets",
+					Namespace: "kube-system",
+				},
+			},
+		},
+		{
+			name: "case 1: no matching secret",
+			obj: &v1alpha1.App{
+				Spec: v1alpha1.AppSpec{
+					Config: v1alpha1.AppSpecConfig{
+						Secret: v1alpha1.AppSpecConfigSecret{
+							Name:      "app-values",
+							Namespace: "default",
+						},
+					},
+					Namespace: "kube-system",
+				},
+			},
+			secret: &corev1.Secret{
+				StringData: map[string]string{
+					"key": "value",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "other-app-values",
+					Namespace: "default",
+				},
+			},
+			expectedSecret: nil,
+		},
+		{
+			name: "case 2: namespace does not match",
+			obj: &v1alpha1.App{
+				Spec: v1alpha1.AppSpec{
+					Config: v1alpha1.AppSpecConfig{
+						Secret: v1alpha1.AppSpecConfigSecret{
+							Name:      "app-values",
+							Namespace: "default",
+						},
+					},
+					Namespace: "kube-system",
+				},
+			},
+			secret: &corev1.Secret{
+				StringData: map[string]string{
+					"key": "value",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "app-values",
+					Namespace: "default",
+				},
+			},
+			expectedSecret: nil,
+		},
+		{
+			name: "case 3: no secrets",
+			obj: &v1alpha1.App{
+				Spec: v1alpha1.AppSpec{
+					Config: v1alpha1.AppSpecConfig{
+						Secret: v1alpha1.AppSpecConfigSecret{
+							Name:      "app-values",
+							Namespace: "default",
+						},
+					},
+				},
+			},
+			expectedSecret: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			objs := make([]runtime.Object, 0, 0)
+			if tc.secret != nil {
+				objs = append(objs, tc.secret)
+			}
+
+			g8sClient := fake.NewSimpleClientset()
+			k8sClient := clientgofake.NewSimpleClientset(objs...)
+
+			var ctx context.Context
+			{
+				c := controllercontext.Context{
+					G8sClient: g8sClient,
+					K8sClient: k8sClient,
+				}
+				ctx = controllercontext.NewContext(context.Background(), c)
+			}
+
+			c := Config{
+				G8sClient: g8sClient,
+				K8sClient: k8sClient,
+				Logger:    microloggertest.New(),
+
+				ProjectName:    "app-operator",
+				WatchNamespace: "default",
+			}
+			r, err := New(c)
+			if err != nil {
+				t.Fatalf("error == %#v, want nil", err)
+			}
+
+			result, err := r.GetCurrentState(ctx, tc.obj)
+			switch {
+			case err != nil && tc.errorMatcher == nil:
+				t.Fatalf("error == %#v, want nil", err)
+			case err == nil && tc.errorMatcher != nil:
+				t.Fatalf("error == nil, want non-nil")
+			case err != nil && !tc.errorMatcher(err):
+				t.Fatalf("error == %#v, want matching", err)
+			}
+
+			secret, err := toSecret(result)
+			if err != nil {
+				t.Fatalf("error == %#v, want nil", err)
+			}
+
+			if !reflect.DeepEqual(secret, tc.expectedSecret) {
+				t.Fatalf("want matching secret \n %s", cmp.Diff(secret, tc.expectedSecret))
+			}
+		})
+	}
+}
