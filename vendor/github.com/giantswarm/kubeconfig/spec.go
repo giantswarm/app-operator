effd4a3379685dcdbe79b5269f48918be08ff4cf
jihyuk <jhbok@giantswarm.io>
2019-03-12 16:19:14 +0100

    update vendor

--- vendor/github.com/giantswarm/kubeconfig/spec.go
+++ vendor/github.com/giantswarm/kubeconfig/spec.go
@@ -11,4 +11,8 @@ type Interface interface {
 	// NewRESTConfigForApp returns a Kubernetes REST Config for the cluster configured
 	// in the kubeconfig section of the app CR.
 	NewRESTConfigForApp(ctx context.Context, app v1alpha1.App) (*rest.Config, error)
+	// NewKubeConfigForRESTConfig returns a kubeConfig bytes for the given REST Config.
+	NewKubeConfigForRESTConfig(ctx context.Context, config *rest.Config, clusterName, namespace string) ([]byte, error)
+	// NewRESTConfigForKubeConfig returns a REST Config for the given KubeConfigValue.
+	NewRESTConfigForKubeConfig(ctx context.Context, kubeConfig []byte) (*rest.Config, error)
 }
