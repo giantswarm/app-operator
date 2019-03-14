4345bf95e6ceeff46ecd62aedb7a1287c6f70b28
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-03-14 10:59:45 +0100

    Add support for kubeconfig flag (#67)
    * Add kubeconfig flag
    
    * Fix type in flag
    
    * vendor: updated operatorkit
    
    * vendor: updated deps
    
    * Use operatorkit flags
    
    * vendor: updated operatorkit
    
    * vendor: updated operatorkit
    
    * Updated rest config wiring
    
    * fix imports
    
    * dep: remove pin

--- flag/service/service.go
+++ flag/service/service.go
@@ -1,9 +1,10 @@
 package service
 
 import (
+	"github.com/giantswarm/operatorkit/flag/service/kubernetes"
+
 	"github.com/giantswarm/app-operator/flag/service/appcatalog"
 	"github.com/giantswarm/app-operator/flag/service/chart"
-	"github.com/giantswarm/app-operator/flag/service/kubernetes"
 )
 
 // Service is an intermediate data structure for command line configuration flags.
