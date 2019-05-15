f7863fb58de77b759e8438d1012f548a698da7c6
jihyuk <jhbok@giantswarm.io>
2019-05-15 09:45:57 +0200

    make it logs more consistent with above logs

--- service/controller/app/v1/resource/chart/current.go
+++ service/controller/app/v1/resource/chart/current.go
@@ -33,7 +33,7 @@ func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interf
 		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("did not find chart %#q in namespace %#q", name, r.chartNamespace))
 		return nil, nil
 	} else if tenant.IsAPINotAvailable(err) {
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("could not find chart %#q in namespace %#q", name, r.chartNamespace))
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("did not find chart %#q in namespace %#q", name, r.chartNamespace))
 		return nil, nil
 	} else if err != nil {
 		return nil, microerror.Mask(err)
