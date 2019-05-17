4d48256a0fbea9f929037f2fc4c2cc93acf7f7dd
jihyuk <jhbok@giantswarm.io>
2019-05-17 18:04:24 +0200

    also add namespace into deletion logic

--- service/controller/app/v1/resource/kubeconfigfinalizer/delete.go
+++ service/controller/app/v1/resource/kubeconfigfinalizer/delete.go
@@ -26,11 +26,11 @@ func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 	name := key.KubecConfigSecretName(cr)
 	namespace := key.KubecConfigSecretNamespace(cr)
 
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("finding kubeconfig secret for app %#q", name))
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("finding kubeconfig secret %#q in namespace %#q", name, namespace))
 
 	kubeConfig, err := r.k8sClient.CoreV1().Secrets(namespace).Get(name, metav1.GetOptions{})
 	if errors.IsNotFound(err) {
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("did not find kubeconfig %#q", name))
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("did not find kubeconfig secret %#q in namespace %#q", name, namespace))
 		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
 		return nil
 
@@ -38,12 +38,12 @@ func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 		return microerror.Mask(err)
 	}
 
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found kubeconfig secret for app %#q", name))
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found kubeconfig secret %#q in namespace %#q", name, namespace))
 
 	finalizerTag := key.KubeConfigFinalizer(cr)
 
 	if contains(kubeConfig.Finalizers, finalizerTag) {
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("clear finalizer for kubeconfig %#q", name))
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("clear finalizer for kubeconfig secret %#q in namespace %#q", name, namespace))
 
 		kubeConfig.Finalizers = filter(kubeConfig.Finalizers, finalizerTag)
 
@@ -52,7 +52,7 @@ func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 			return microerror.Mask(err)
 		}
 	} else {
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("kubeconfig %#q do not have matching finalizer", name))
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("kubeconfig secret %#q in namespace %#q do not have matching finalizer", name, namespace))
 	}
 	return nil
 }
