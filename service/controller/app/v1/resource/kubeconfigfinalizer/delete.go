0755b88e66f8f74cc4fe4d0560056421dfc987e3
jihyuk <jhbok@giantswarm.io>
2019-05-17 16:47:35 +0200

    ret as variable in filter func

--- service/controller/app/v1/resource/kubeconfigfinalizer/delete.go
+++ service/controller/app/v1/resource/kubeconfigfinalizer/delete.go
@@ -57,7 +57,8 @@ func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 	return nil
 }
 
-func filter(finalizers []string, matching string) (ret []string) {
+func filter(finalizers []string, matching string) []string {
+	var ret []string
 	for _, f := range finalizers {
 		if f != matching {
 			ret = append(ret, f)
