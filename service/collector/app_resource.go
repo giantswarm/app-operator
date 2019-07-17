361639d875e39e85a711d4f54a7bcc8c78592aec
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2019-07-17 15:53:31 +0200

    Adding app cordon metrics (#106)
    * add app cordon metrics
    
    * fmt
    
    * ordering methods
    
    * err handling fixed in collector
    
    * better var naming
    
    * adding more unittest

--- service/collector/app_resource.go
+++ service/collector/app_resource.go
@@ -3,6 +3,8 @@ package collector
 import (
 	"context"
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
 	"github.com/giantswarm/microerror"
@@ -10,17 +12,28 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+
+	"github.com/giantswarm/app-operator/service/controller/app/v1/key"
 )
 
 var (
-	appDesc *prometheus.Desc = prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, "", "app_info"),
+	appDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "app", "info"),
 		"Managed apps status.",
 		[]string{
 			labelName,
 			labelNamespace,
 			labelStatus,
 			labelVersion,
+		},
+		nil,
+	)
+
+	appCordonExpireTimeDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "app", "cordon_expire_time_seconds"),
+		"A metric of the expire time of cordoned apps unix seconds.",
+		[]string{
+			labelName,
 		},
 		nil,
 	)
@@ -61,11 +74,32 @@ func NewAppResource(config AppResourceConfig) (*AppResource, error) {
 	return c, nil
 }
 
-func (c *AppResource) collectAppStatus(ctx context.Context, ch chan<- prometheus.Metric) {
+// Collect is the main metrics collection function.
+func (c *AppResource) Collect(ch chan<- prometheus.Metric) error {
+	ctx := context.Background()
+
+	c.logger.LogCtx(ctx, "level", "debug", "message", "collecting metrics")
+
+	err := c.collectAppStatus(ctx, ch)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	c.logger.LogCtx(ctx, "level", "debug", "message", "finished collecting metrics")
+	return nil
+}
+
+// Describe emits the description for the metrics collected here.
+func (c *AppResource) Describe(ch chan<- *prometheus.Desc) error {
+	ch <- appDesc
+	ch <- appCordonExpireTimeDesc
+	return nil
+}
+
+func (c *AppResource) collectAppStatus(ctx context.Context, ch chan<- prometheus.Metric) error {
 	r, err := c.g8sClient.ApplicationV1alpha1().Apps("").List(metav1.ListOptions{})
 	if err != nil {
-		c.logger.LogCtx(ctx, "level", "error", "message", "could not get apps", "stack", fmt.Sprintf("%#v", err))
-		return
+		return microerror.Mask(err)
 	}
 
 	for _, app := range r.Items {
@@ -79,24 +113,38 @@ func (c *AppResource) collectAppStatus(ctx context.Context, ch chan<- prometheus
 			app.Status.Version,
 		)
 
+		if !key.IsCordoned(app) {
+			continue
+		}
+
+		t, err := convertToTime(key.CordonUntil(app))
+		if err != nil {
+			c.logger.Log("level", "warning", "message", "could not convert cordon-until", "stack", fmt.Sprintf("%#v", err))
+			continue
+		}
+
+		ch <- prometheus.MustNewConstMetric(
+			appCordonExpireTimeDesc,
+			prometheus.GaugeValue,
+			float64(t.Unix()),
+			key.AppName(app),
+		)
+	}
+	return nil
+}
+
+func convertToTime(input string) (time.Time, error) {
+	layout := "2006-01-02T15:04:05"
+
+	split := strings.Split(input, ".")
+	if len(split) == 0 {
+		return time.Time{}, microerror.Maskf(invalidExecutionError, "%#q must have at least one item in order to collect metrics for the cordon expiration", input)
 	}
 
-}
+	t, err := time.Parse(layout, split[0])
+	if err != nil {
+		return time.Time{}, microerror.Maskf(invalidExecutionError, "parsing timestamp %#q failed: %#v", split[0], err.Error())
+	}
 
-// Collect is the main metrics collection function.
-func (c *AppResource) Collect(ch chan<- prometheus.Metric) error {
-	ctx := context.Background()
-
-	c.logger.LogCtx(ctx, "level", "debug", "message", "collecting metrics")
-
-	c.collectAppStatus(ctx, ch)
-
-	c.logger.LogCtx(ctx, "level", "debug", "message", "finished collecting metrics")
-	return nil
-}
-
-// Describe emits the description for the metrics collected here.
-func (c *AppResource) Describe(ch chan<- *prometheus.Desc) error {
-	ch <- appDesc
-	return nil
+	return t, nil
 }
