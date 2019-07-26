9d1323c3044f4350d9243c0882d766b5895fabd0
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2019-07-26 10:16:30 +0200

    Update service/controller/app/v1/resource/appcatalog/resource.go
    Co-Authored-By: Ross Fairbanks <rossf7@users.noreply.github.com>

--- service/controller/app/v1/resource/appcatalog/resource.go
+++ service/controller/app/v1/resource/appcatalog/resource.go
@@ -64,7 +64,7 @@ func (r *Resource) getCatalogForApp(ctx context.Context, customResource v1alpha1
 
 	catalogName := key.CatalogName(customResource)
 
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("looking for appCatalog %#q in namespace %#q", catalogName, "default"))
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("looking for appCatalog %#q", catalogName))
 
 	appCatalog, err := r.g8sClient.ApplicationV1alpha1().AppCatalogs().Get(catalogName, metav1.GetOptions{})
 	if apierrors.IsNotFound(err) {
