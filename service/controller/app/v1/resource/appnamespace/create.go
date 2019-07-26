6291bb78b5d023b27fdb3353cf150f655fa0619f
jihyuk <jhbok@giantswarm.io>
2019-07-26 10:29:49 +0200

    address review 2

--- service/controller/app/v1/resource/appnamespace/create.go
+++ service/controller/app/v1/resource/appnamespace/create.go
@@ -2,6 +2,8 @@ package appnamespace
 
 import "context"
 
+// EnsureCreated is not implemented because we only need to consider
+// the status of the namespace on deletion events.
 func (*Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	return nil
 }
