63cd60eed65e89cddd591c46eb5b7d45163a012e
jihyuk <jhbok@giantswarm.io>
2019-08-30 15:26:13 +0200

    adding test values for kubeconfig test too

--- integration/test/app/kubeconfig/kubeconfig_test.go
+++ integration/test/app/kubeconfig/kubeconfig_test.go
@@ -68,7 +68,11 @@ func TestAppLifecycleUsingKubeconfig(t *testing.T) {
 		},
 		Namespace: namespace,
 		ConfigMap: chartvalues.APIExtensionsAppE2EConfigConfigMap{
-			ValuesYAML: `test: "values"`,
+			ValuesYAML: `test:
+      image:
+        registry: quay.io
+        repository: giantswarm/alpine-testing
+        tag: 0.1.1`,
 		},
 		Secret: chartvalues.APIExtensionsAppE2EConfigSecret{
 			ValuesYAML: `test: "secret"`,
