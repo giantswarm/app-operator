023a5e57fe7f5d4946e2e0cc9d3e1f7d73dce104
Ross Fairbanks <ross@rossfairbanks.com>
2019-09-19 10:32:45 +0200

    fix merge

--- integration/templates/chart_operator_values.go
+++ integration/templates/chart_operator_values.go
@@ -5,11 +5,8 @@ package templates
 // ChartOperatorValues values required by chart-operator-chart.
 const ChartOperatorValues = `
 clusterDNSIP: 10.96.0.10
-<<<<<<< HEAD
-=======
 image:
   registry: "quay.io"
->>>>>>> master
 tiller:
   namespace: "giantswarm"
 `
