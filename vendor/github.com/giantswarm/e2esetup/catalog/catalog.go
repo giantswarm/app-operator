4912da17dff78cc3ad85af351a9fb80cf91dec3c
jihyuk <jhbok@giantswarm.io>
2019-10-11 11:09:22 +0200

    update vendor

--- vendor/github.com/giantswarm/e2esetup/catalog/catalog.go
+++ vendor/github.com/giantswarm/e2esetup/catalog/catalog.go
@@ -4,28 +4,61 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/ghodss/yaml"
 	"github.com/giantswarm/microerror"
-	"github.com/google/go-github/github"
+	"github.com/levigross/grequests"
 )
 
+// GetLatestChart returns the latest chart tarball file in the specified catalog.
 func GetLatestChart(ctx context.Context, catalog, app string) (string, error) {
-	client := github.NewClient(nil)
-
-	query := fmt.Sprintf("repo:giantswarm/%s filename:%s", catalog, app)
-	searchOption := github.SearchOptions{
-		Sort: "indexed",
-	}
-	result, _, err := client.Search.Code(ctx, query, &searchOption)
+	index, err := getIndex(catalog)
 	if err != nil {
 		return "", microerror.Mask(err)
 	}
 
-	path := result.CodeResults[0].GetPath()
+	var downloadURL string
+	{
+		entry, ok := index.Entries[app]
+		if !ok {
+			return "", microerror.Maskf(notFoundError, fmt.Sprintf("no app %q in index.yaml", app))
+		}
+		downloadURL = entry[0].Urls[0]
+	}
 
-	r, _, _, err := client.Repositories.GetContents(ctx, "giantswarm", catalog, path, nil)
+	return downloadURL, nil
+}
+
+// GetLatestTag returns the latest tag in the specified catalog.
+func GetLatestTag(ctx context.Context, catalog, app string) (string, error) {
+	index, err := getIndex(catalog)
 	if err != nil {
 		return "", microerror.Mask(err)
 	}
 
-	return r.GetDownloadURL(), nil
+	var version string
+	{
+		entry, ok := index.Entries[app]
+		if !ok {
+			return "", microerror.Maskf(notFoundError, fmt.Sprintf("no app %q in index.yaml", app))
+		}
+		version = entry[0].Version
+	}
+
+	return version, nil
+}
+
+func getIndex(catalog string) (*Index, error) {
+	indexURL := fmt.Sprintf("https://giantswarm.github.io/%s/index.yaml", catalog)
+	resp, err := grequests.Get(indexURL, nil)
+	if err != nil {
+		return &Index{}, microerror.Mask(err)
+	}
+
+	var index Index
+	err = yaml.Unmarshal(resp.Bytes(), &index)
+	if err != nil {
+		return &Index{}, microerror.Mask(err)
+	}
+
+	return &index, nil
 }
---
+++ vendor/github.com/giantswarm/e2esetup/catalog/error.go
@@ -0,0 +1,12 @@
+package catalog
+
+import "github.com/giantswarm/microerror"
+
+var notFoundError = &microerror.Error{
+	Kind: "notFoundError",
+}
+
+// IsNotFound asserts notFoundError.
+func IsNotFound(err error) bool {
+	return microerror.Cause(err) == notFoundError
+}
