f2305facc2c4e57c483064c5c0449d87a4456bf0
Ross Fairbanks <ross@rossfairbanks.com>
2019-12-28 10:15:48 +0000

    Add tcnamespace resource

---
+++ service/controller/app/v1/resource/tcnamespace/resource.go
@@ -0,0 +1,37 @@
+package tcnamespace
+
+import (
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+)
+
+const (
+	Name = "tcnamespacev1"
+
+	namespace = "giantswarm"
+)
+
+type Config struct {
+	Logger micrologger.Logger
+}
+
+type Resource struct {
+	logger micrologger.Logger
+}
+
+// New creates a new configured tcnamespace resource.
+func New(config Config) (*Resource, error) {
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+
+	r := &Resource{
+		logger: config.Logger,
+	}
+
+	return r, nil
+}
+
+func (r Resource) Name() string {
+	return Name
+}
