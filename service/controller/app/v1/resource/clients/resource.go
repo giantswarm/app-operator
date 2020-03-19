294fb3b0258e059cff1c9379b216fb34a461001d
Ross Fairbanks <ross@rossfairbanks.com>
2020-03-19 12:31:44 +0100

    Updated clients resource

--- service/controller/app/v1/resource/clients/resource.go
+++ service/controller/app/v1/resource/clients/resource.go
@@ -2,15 +2,17 @@ package clients
 
 import (
 	"context"
-	"time"
 
 	"github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1"
-	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"github.com/giantswarm/errors/tenant"
 	"github.com/giantswarm/helmclient"
+	"github.com/giantswarm/k8sclient"
 	"github.com/giantswarm/kubeconfig"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
+	"github.com/spf13/afero"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 
 	"github.com/giantswarm/app-operator/service/controller/app/v1/controllercontext"
 )
@@ -114,32 +116,36 @@ func (r *Resource) addClientsToContext(ctx context.Context, cr v1alpha1.App) err
 		return microerror.Mask(err)
 	}
 
-	var g8sClient versioned.Interface
+	var k8sClient k8sclient.Interface
 	{
-		g8sClient, err = versioned.NewForConfig(restConfig)
-		if err != nil {
+		c := k8sclient.ClientsConfig{
+			Logger:     r.logger,
+			RestConfig: rest.CopyConfig(restConfig),
+		}
+
+		k8sClient, err = k8sclient.NewClients(c)
+		if tenant.IsAPINotAvailable(err) {
+			// Set status so we don't try to connect to the tenant cluster
+			// again in this reconciliation loop.
+			cc.Status.TenantCluster.IsUnavailable = true
+
+			r.logger.LogCtx(ctx, "level", "debug", "message", "tenant API not available yet")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+			return nil
+
+		} else if err != nil {
 			return microerror.Mask(err)
 		}
 	}
 
-	var k8sClient kubernetes.Interface
-	{
-		k8sClient, err = kubernetes.NewForConfig(restConfig)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-	}
+	fs := afero.NewOsFs()
 
 	var helmClient helmclient.Interface
 	{
 		c := helmclient.Config{
+			Fs:        fs,
 			K8sClient: k8sClient,
 			Logger:    r.logger,
-
-			EnsureTillerInstalledMaxWait: 30 * time.Second,
-			RestConfig:                   restConfig,
-			TillerImageRegistry:          r.imageRegistry,
-			TillerNamespace:              r.tillerNamespace,
 		}
 
 		helmClient, err = helmclient.New(c)
@@ -149,8 +155,8 @@ func (r *Resource) addClientsToContext(ctx context.Context, cr v1alpha1.App) err
 	}
 
 	cc.Clients = controllercontext.Clients{
-		G8s:  g8sClient,
-		K8s:  k8sClient,
+		G8s:  k8sClient.G8sClient(),
+		K8s:  k8sClient.K8sClient(),
 		Helm: helmClient,
 	}
 
