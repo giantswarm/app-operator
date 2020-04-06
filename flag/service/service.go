2724742f0865b86fb2f4ee868ea1b91955c40a93
jihyuk <jhbok@giantswarm.io>
2020-04-06 17:09:36 +0200

    wiring

--- flag/service/service.go
+++ flag/service/service.go
@@ -4,12 +4,14 @@ import (
 	"github.com/giantswarm/operatorkit/flag/service/kubernetes"
 
 	"github.com/giantswarm/app-operator/flag/service/chart"
+	"github.com/giantswarm/app-operator/flag/service/helm"
 	"github.com/giantswarm/app-operator/flag/service/image"
 )
 
 // Service is an intermediate data structure for command line configuration flags.
 type Service struct {
 	Chart      chart.Chart
+	Helm       helm.Helm
 	Image      image.Image
 	Kubernetes kubernetes.Kubernetes
 }
