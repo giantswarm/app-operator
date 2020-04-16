19a8df83b6538bc1502b851775af82a93f4242c6
Ross Fairbanks <rossf7@users.noreply.github.com>
2020-04-16 15:33:12 +0200

    Remove hardcoded release name from chartoperator resource (#259)
    * Remove hardcoded release name from chartoperator resource
    
    * Fix integration test by disabling chart-operator priority class
    
    * Remove hardcoded release name

--- service/controller/app/v1/resource/chartoperator/resource.go
+++ service/controller/app/v1/resource/chartoperator/resource.go
@@ -27,11 +27,6 @@ const (
 	Name = "chartoperatorv1"
 )
 
-const (
-	namespace = "giantswarm"
-	release   = "chart-operator"
-)
-
 // Config represents the configuration used to create a new clients resource.
 type Config struct {
 	// Dependencies.
@@ -124,7 +119,7 @@ func (r Resource) installChartOperator(ctx context.Context, cr v1alpha1.App) err
 	}
 
 	{
-		err = cc.Clients.Helm.InstallReleaseFromTarball(ctx, tarballPath, namespace, helm.ReleaseName(release), helm.ValueOverrides(chartOperatorValues))
+		err = cc.Clients.Helm.InstallReleaseFromTarball(ctx, tarballPath, key.Namespace(cr), helm.ReleaseName(cr.Name), helm.ValueOverrides(chartOperatorValues))
 		if err != nil {
 			return microerror.Mask(err)
 		}
@@ -134,10 +129,10 @@ func (r Resource) installChartOperator(ctx context.Context, cr v1alpha1.App) err
 		// We wait for the chart-operator deployment to be ready so the
 		// chart CRD is installed. This allows the chart
 		// resource to create CRs in the same reconcilation loop.
-		r.logger.LogCtx(ctx, "level", "debug", "message", "waiting for ready chart-operator deployment")
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("waiting for ready %#q deployment", cr.Name))
 
 		o := func() error {
-			err := r.checkDeploymentReady(ctx, cc.Clients.K8s)
+			err := r.checkDeploymentReady(ctx, cc.Clients.K8s, cr)
 			if err != nil {
 				return microerror.Mask(err)
 			}
@@ -150,7 +145,7 @@ func (r Resource) installChartOperator(ctx context.Context, cr v1alpha1.App) err
 		// reconciliation loop.
 		b := backoff.NewConstant(20*time.Second, 5*time.Second)
 		n := func(err error, delay time.Duration) {
-			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("%#q deployment is not ready retrying in %s", release, delay), "stack", fmt.Sprintf("%#v", err))
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("%#q deployment is not ready retrying in %s", cr.Name, delay), "stack", fmt.Sprintf("%#v", err))
 		}
 
 		err = backoff.RetryNotify(o, b, n)
@@ -158,7 +153,7 @@ func (r Resource) installChartOperator(ctx context.Context, cr v1alpha1.App) err
 			return microerror.Mask(err)
 		}
 
-		r.logger.LogCtx(ctx, "level", "debug", "message", "chart-operator deployment is ready")
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("%#q deployment is ready", cr.Name))
 	}
 
 	return nil
@@ -200,17 +195,17 @@ func (r Resource) updateChartOperator(ctx context.Context, cr v1alpha1.App) erro
 	}
 
 	{
-		err = cc.Clients.Helm.UpdateReleaseFromTarball(ctx, release, tarballPath, helm.UpdateValueOverrides(chartOperatorValues), helm.UpgradeForce(true))
+		err = cc.Clients.Helm.UpdateReleaseFromTarball(ctx, cr.Name, tarballPath, helm.UpdateValueOverrides(chartOperatorValues), helm.UpgradeForce(true))
 		if err != nil {
 			return microerror.Mask(err)
 		}
 	}
 
 	{
-		r.logger.LogCtx(ctx, "level", "debug", "message", "waiting for ready chart-operator deployment")
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("waiting for ready %#q deployment", cr.Name))
 
 		o := func() error {
-			err := r.checkDeploymentReady(ctx, cc.Clients.K8s)
+			err := r.checkDeploymentReady(ctx, cc.Clients.K8s, cr)
 			if err != nil {
 				return microerror.Mask(err)
 			}
@@ -220,7 +215,7 @@ func (r Resource) updateChartOperator(ctx context.Context, cr v1alpha1.App) erro
 
 		b := backoff.NewConstant(20*time.Second, 10*time.Second)
 		n := func(err error, delay time.Duration) {
-			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("%#q deployment is not ready retrying in %s", release, delay), "stack", fmt.Sprintf("%#v", err))
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("%#q deployment is not ready retrying in %s", cr.Name, delay), "stack", fmt.Sprintf("%#v", err))
 		}
 
 		err = backoff.RetryNotify(o, b, n)
@@ -228,7 +223,7 @@ func (r Resource) updateChartOperator(ctx context.Context, cr v1alpha1.App) erro
 			return microerror.Mask(err)
 		}
 
-		r.logger.LogCtx(ctx, "level", "debug", "message", "chart-operator deployment is ready")
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("%#q deployment is ready", cr.Name))
 	}
 
 	return nil
@@ -252,16 +247,17 @@ func (r *Resource) mergeChartOperatorValues(ctx context.Context, cr v1alpha1.App
 
 // checkDeploymentReady checks for the specified deployment that the number of
 // ready replicas matches the desired state.
-func (r *Resource) checkDeploymentReady(ctx context.Context, k8sClient kubernetes.Interface) error {
-	deploy, err := k8sClient.AppsV1().Deployments(namespace).Get(release, metav1.GetOptions{})
+func (r *Resource) checkDeploymentReady(ctx context.Context, k8sClient kubernetes.Interface, cr v1alpha1.App) error {
+	namespace := key.Namespace(cr)
+	deploy, err := k8sClient.AppsV1().Deployments(namespace).Get(cr.Name, metav1.GetOptions{})
 	if apierrors.IsNotFound(err) {
-		return microerror.Maskf(notReadyError, "deployment %#q not found", release)
+		return microerror.Maskf(notReadyError, "deployment %#q not found", cr.Name)
 	} else if err != nil {
 		return microerror.Mask(err)
 	}
 
 	if deploy.Status.ReadyReplicas != *deploy.Spec.Replicas {
-		return microerror.Maskf(notReadyError, "deployment %#q want %d replicas %d ready", release, *deploy.Spec.Replicas, deploy.Status.ReadyReplicas)
+		return microerror.Maskf(notReadyError, "deployment %#q want %d replicas %d ready", cr.Name, *deploy.Spec.Replicas, deploy.Status.ReadyReplicas)
 	}
 
 	// Deployment is ready.
