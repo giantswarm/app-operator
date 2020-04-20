9c354f8f16a250567611623f6cbde0622a990e08
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2020-04-20 18:27:18 +0200

    Remove v1 resourceset (#265)
    * remove v1 subdirectory
    
    * remove v1 subdirectory (1)
    
    * adop package versionbundle
    
    * adding appVersion and move version as 1.0.0-dev
    
    * Update pkg/project/project.go
    
    Co-Authored-By: Ross Fairbanks <rossf7@users.noreply.github.com>
    
    Co-authored-by: Ross Fairbanks <rossf7@users.noreply.github.com>

--- service/controller/app/app.go
+++ service/controller/app/app.go
@@ -10,7 +10,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 
 	"github.com/giantswarm/app-operator/pkg/project"
-	v1 "github.com/giantswarm/app-operator/service/controller/app/v1"
 )
 
 type Config struct {
@@ -45,7 +44,7 @@ func NewApp(config Config) (*App, error) {
 
 	var resourceSetV1 *controller.ResourceSet
 	{
-		c := v1.ResourceSetConfig{
+		c := ResourceSetConfig{
 			ChartNamespace: config.ChartNamespace,
 			FileSystem:     config.Fs,
 			ImageRegistry:  config.ImageRegistry,
@@ -53,7 +52,7 @@ func NewApp(config Config) (*App, error) {
 			Logger:         config.Logger,
 		}
 
-		resourceSetV1, err = v1.NewResourceSet(c)
+		resourceSetV1, err = NewResourceSet(c)
 		if err != nil {
 			return nil, microerror.Mask(err)
 		}
---
+++ service/controller/app/resource/appnamespace/delete.go
@@ -0,0 +1,25 @@
+package appnamespace
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/app-operator/service/controller/app/key"
+)
+
+// EnsureDeleted checks whether the namespace this app CR belongs to
+// is currently being deleted.
+func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
+	cr, err := key.ToCustomResource(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	err = r.addNamespaceStatusToContext(ctx, cr)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
