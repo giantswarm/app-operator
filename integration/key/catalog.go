c59ba35efde5cd4a14e1973ca56088e1c8623acd
Jihyuk Bok <jhbok@giantswarm.io>
2020-05-05 14:23:26 +0200

    fix the test build error

--- integration/key/catalog.go
+++
@@ -1,9 +0,0 @@
-package key
-
-func DefaultCatalogStorageURL() string {
-	return "https://giantswarm.github.com/default-catalog"
-}
-
-func ControlPlaneTestCatalogStorageURL() string {
-	return "https://giantswarm.github.io/control-plane-test-catalog"
-}
