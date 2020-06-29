acf9587701c7403e72bfb8444658bcbacafa3c1b
Jihyuk Bok <jhbok@giantswarm.io>
2020-06-29 11:37:54 +0200

    change the project version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-operator"
 	source      = "https://github.com/giantswarm/app-operator"
-	version     = "1.1.5"
+	version     = "1.1.6"
 )
 
 // AppControlPlaneVersion is always 0.0.0 for control plane app CRs. These CRs
