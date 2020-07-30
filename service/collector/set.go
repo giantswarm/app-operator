31677d4725403b84f1242c5d135438724cd3ea71
Ross Fairbanks <rossf7@users.noreply.github.com>
2020-07-30 10:21:10 +0200

    Improve app collector (#436)
    * Improve app resource
    
    * Add ignore for linter

--- service/collector/set.go
+++ service/collector/set.go
@@ -35,9 +35,8 @@ func NewSet(config SetConfig) (*Set, error) {
 
 	var appCollector *App
 	{
-		c := AppConfig{
-			G8sClient: config.K8sClient.G8sClient(),
-			K8sClient: config.K8sClient.K8sClient(),
+		c := AppConfig{ // nolint:gosimple
+			K8sClient: config.K8sClient,
 			Logger:    config.Logger,
 
 			AppTeamMapping: config.AppTeamMapping,
