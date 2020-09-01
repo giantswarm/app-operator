405725c2bb334637768e45194ab0e5f6d8b2b222
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2020-09-01 14:47:11 +0200

    bugfix; Update status when configmap/secret failed to merge initially (#470)
    * fix-status
    
    * changelog
    
    * Update CHANGELOG.md
    
    Co-authored-by: Ross Fairbanks <rossf7@users.noreply.github.com>
    
    * newline
    
    Co-authored-by: Ross Fairbanks <rossf7@users.noreply.github.com>

--- service/controller/app/resource/status/create.go
+++ service/controller/app/resource/status/create.go
@@ -37,27 +37,8 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		return nil
 	}
 
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("finding status for chart %#q in namespace %#q", cr.Name, r.chartNamespace))
-
-	chart, err := cc.Clients.K8s.G8sClient().ApplicationV1alpha1().Charts(r.chartNamespace).Get(ctx, cr.Name, metav1.GetOptions{})
-	if apierrors.IsNotFound(err) {
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("did not find chart %#q in namespace %#q", cr.Name, r.chartNamespace))
-		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
-		return nil
-	} else if tenant.IsAPINotAvailable(err) {
-		// We should not hammer tenant API if it is not available, the tenant cluster
-		// might be initializing. We will retry on next reconciliation loop.
-		r.logger.LogCtx(ctx, "level", "debug", "message", "tenant cluster is not available.")
-		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
-		return nil
-	} else if err != nil {
-		return microerror.Mask(err)
-	}
-
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found status for chart %#q in namespace %#q", cr.Name, r.chartNamespace))
-
-	chartStatus := key.ChartStatus(*chart)
 	var desiredStatus v1alpha1.AppStatus
+
 	if cc.Status.ChartStatus.Status != "" {
 		desiredStatus = v1alpha1.AppStatus{
 			Release: v1alpha1.AppStatusRelease{
@@ -66,6 +47,26 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 			},
 		}
 	} else {
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("finding status for chart %#q in namespace %#q", cr.Name, r.chartNamespace))
+
+		chart, err := cc.Clients.K8s.G8sClient().ApplicationV1alpha1().Charts(r.chartNamespace).Get(ctx, cr.Name, metav1.GetOptions{})
+		if apierrors.IsNotFound(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("did not find chart %#q in namespace %#q", cr.Name, r.chartNamespace))
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+			return nil
+		} else if tenant.IsAPINotAvailable(err) {
+			// We should not hammer tenant API if it is not available, the tenant cluster
+			// might be initializing. We will retry on next reconciliation loop.
+			r.logger.LogCtx(ctx, "level", "debug", "message", "tenant cluster is not available.")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+			return nil
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found status for chart %#q in namespace %#q", cr.Name, r.chartNamespace))
+
+		chartStatus := key.ChartStatus(*chart)
 		desiredStatus = v1alpha1.AppStatus{
 			AppVersion: chartStatus.AppVersion,
 			Release: v1alpha1.AppStatusRelease{
