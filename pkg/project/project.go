23d79294be41e23bdea384cbc801128018bdc385
Ross Fairbanks <ross@rossfairbanks.com>
2020-09-07 11:34:21 +0200

    helm2: Release v1.1.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-operator"
 	source      = "https://github.com/giantswarm/app-operator"
-	version     = "1.0.10-dev"
+	version     = "1.1.0"
 )
 
 // AppControlPlaneVersion is always 0.0.0 for control plane app CRs. These CRs
