125a2df181c62ce2980c395fa2d5fe2abf60797b
Ross Fairbanks <ross@rossfairbanks.com>
2020-09-07 11:58:03 +0200

    helm2: Use v1.0.10 not v1.1.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-operator"
 	source      = "https://github.com/giantswarm/app-operator"
-	version     = "1.1.0"
+	version     = "1.0.10"
 )
 
 // AppControlPlaneVersion is always 0.0.0 for control plane app CRs. These CRs
