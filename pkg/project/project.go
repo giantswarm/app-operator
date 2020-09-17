cca3b6563ddbc5c8fcf1ba81a67707f2c3e418aa
Ross Fairbanks <ross@rossfairbanks.com>
2020-09-17 15:34:53 +0200

    Release v2.3.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-operator"
 	source      = "https://github.com/giantswarm/app-operator"
-	version     = "2.2.0"
+	version     = "2.3.0"
 )
 
 // AppControlPlaneVersion is always 0.0.0 for control plane app CRs. These CRs
