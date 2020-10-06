3f1928b618262aff4d56b4e14b789ce42842d212
Ross Fairbanks <ross@rossfairbanks.com>
2020-10-06 17:25:57 +0200

    Use v0.3.0 release of apptestctl

--- integration/setup/setup.sh
+++ integration/setup/setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-go get github.com/giantswarm/apptestctl@operator-flag
+curl -L https://github.com/giantswarm/apptestctl/releases/download/v0.3.0/apptestctl-v0.3.0-linux-amd64.tar.gz > /tmp/apptestctl.tar.gz
+cd /tmp
+tar xzvf apptestctl.tar.gz
+chmod u+x /tmp/apptestctl-v0.3.0-linux-amd64/apptestctl
+sudo mv /tmp/apptestctl-v0.3.0-linux-amd64/apptestctl /usr/local/bin
+
+# Don't install app-operator or chart-operator this is done by the test setup.
 apptestctl bootstrap --install-operators=false --kubeconfig="$(kind get kubeconfig)"
