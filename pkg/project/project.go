0a58a781dd1ce3998de8d18ac31e2dee15b967c8
GitHub Action <action@github.com>
2020-10-26 12:03:21 +0000

    Bump version to 2.4.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-operator"
 	source      = "https://github.com/giantswarm/app-operator"
-	version     = "2.4.1"
+	version     = "2.4.2-dev"
 )
 
 // AppControlPlaneVersion is always 0.0.0 for control plane app CRs. These CRs
