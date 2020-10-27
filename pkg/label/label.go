ac3d4eec676d9c59e55e40f1df1b020122d1ceed
Ross Fairbanks <ross@rossfairbanks.com>
2020-10-27 10:15:37 +0100

    Remove code

--- pkg/label/label.go
+++ pkg/label/label.go
@@ -11,34 +11,6 @@ import (
 	"github.com/giantswarm/app-operator/v2/pkg/project"
 )
 
-const (
-	// AppKubernetesName label is used to identify Kubernetes resources.
-	AppKubernetesName = "app.kubernetes.io/name"
-
-	// AppKubernetesVersion label is used to identify the version of Kubernetes
-	// resources.
-	AppKubernetesVersion = "app.kubernetes.io/version"
-
-	// CatalogName label is used to identify resources belonging to a Giant Swarm
-	// app catalog.
-	CatalogName = "application.giantswarm.io/catalog"
-
-	// CatalogType label is used to identify the type of Giant Swarm app catalog
-	// e.g. stable or test.
-	CatalogType = "application.giantswarm.io/catalog-type"
-
-	// CatalogVisibility label is used to determine how Giant Swarm app catalogs
-	// are displayed in the UX. e.g. public or internal.
-	CatalogVisibility = "application.giantswarm.io/catalog-visibility"
-
-	// Latest label is added to appcatalogentry CRs to filter for the most
-	// recent release.
-	Latest = "latest"
-
-	// Watching is the label added to configmaps watched by the app value controller.
-	Watching = "app-operator.giantswarm.io/watching"
-)
-
 func AppVersionSelector(unique bool) labels.Selector {
 	version := GetProjectVersion(unique)
 	s := fmt.Sprintf("%s=%s", label.AppOperatorVersion, version)
