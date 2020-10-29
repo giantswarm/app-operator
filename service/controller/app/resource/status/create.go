7979d0c265f4ab1d8811e81ad75b11c0a1592a81
Ross Fairbanks <ross@rossfairbanks.com>
2020-10-29 15:18:05 +0100

    Compiling

--- service/controller/app/resource/status/create.go
+++ service/controller/app/resource/status/create.go
@@ -70,7 +70,7 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		desiredStatus = v1alpha1.AppStatus{
 			AppVersion: chartStatus.AppVersion,
 			Release: v1alpha1.AppStatusRelease{
-				LastDeployed: chartStatus.Release.LastDeployed,
+				LastDeployed: *chartStatus.Release.LastDeployed,
 				Reason:       chartStatus.Reason,
 				Status:       chartStatus.Release.Status,
 			},
