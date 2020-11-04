48081459a731f1b87c71c8e5f6d3a1c64c299704
Jihyuk Bok <jhbok@giantswarm.io>
2020-11-04 17:17:41 +0100

    assign webhook URL to chart CRs even if they are not CP apps

--- service/controller/app/resource/chart/desired.go
+++ service/controller/app/resource/chart/desired.go
@@ -74,17 +74,15 @@ func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interf
 
 	annotations := generateAnnotations(cr.GetAnnotations())
 
-	if key.InCluster(cr) {
-		u, err := url.Parse(r.webhookBaseURL)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-
-		u.Path = path.Join(u.Path, "status", cr.Namespace, cr.Name)
-
-		webhookAnnotation := fmt.Sprintf("%s/%s", annotation.ChartOperatorPrefix, annotation.WebhookURL)
-		annotations[webhookAnnotation] = u.String()
+	u, err := url.Parse(r.webhookBaseURL)
+	if err != nil {
+		return nil, microerror.Mask(err)
 	}
+
+	u.Path = path.Join(u.Path, "status", cr.Namespace, cr.Name)
+
+	webhookAnnotation := fmt.Sprintf("%s/%s", annotation.ChartOperatorPrefix, annotation.WebhookURL)
+	annotations[webhookAnnotation] = u.String()
 
 	if len(annotations) > 0 {
 		chartCR.Annotations = annotations
