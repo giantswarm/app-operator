f91711c03092056f7384684b80a0fcb320506c07
Ross Fairbanks <ross@rossfairbanks.com>
2020-11-09 16:13:29 +0100

    Delete labels extracted to apiextensions

--- pkg/label/label.go
+++ pkg/label/label.go
@@ -12,25 +12,6 @@ import (
 )
 
 const (
-	// AppKubernetesName label is used to identify Kubernetes resources.
-	AppKubernetesName = "app.kubernetes.io/name"
-
-	// AppKubernetesVersion label is used to identify the version of Kubernetes
-	// resources.
-	AppKubernetesVersion = "app.kubernetes.io/version"
-
-	// CatalogName label is used to identify resources belonging to a Giant Swarm
-	// app catalog.
-	CatalogName = "application.giantswarm.io/catalog"
-
-	// CatalogType label is used to identify the type of Giant Swarm app catalog
-	// e.g. stable or test.
-	CatalogType = "application.giantswarm.io/catalog-type"
-
-	// CatalogVisibility label is used to determine how Giant Swarm app catalogs
-	// are displayed in the UX. e.g. public or internal.
-	CatalogVisibility = "application.giantswarm.io/catalog-visibility"
-
 	// Latest label is added to appcatalogentry CRs to filter for the most
 	// recent release.
 	Latest = "latest"
