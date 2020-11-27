06fe6b2bf4577a1b78d1d1a09c3d29365931eb3c
vol <me@volodymyrstoiko.com>
2020-11-27 17:21:43 +0200

    gimport

--- service/controller/app/values/secret.go
+++ service/controller/app/values/secret.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 
 	"github.com/giantswarm/apiextensions/v3/pkg/apis/application/v1alpha1"
-	"github.com/giantswarm/app-operator/v2/pkg/annotation"
 	"github.com/giantswarm/app/v3/pkg/key"
 	"github.com/giantswarm/helmclient/v3/pkg/helmclient"
 	"github.com/giantswarm/microerror"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/giantswarm/app-operator/v2/pkg/annotation"
 )
 
 // MergeSecretData merges the data from the catalog, app and user secretss
