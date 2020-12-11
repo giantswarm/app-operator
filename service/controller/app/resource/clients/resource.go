a6a3b5bbba401a76df9ac705de503aeb3a8d6963
Jihyuk Bok <jhbok@giantswarm.io>
2020-12-11 11:11:54 +0100

    import sorts

--- service/controller/app/resource/clients/resource.go
+++ service/controller/app/resource/clients/resource.go
@@ -2,10 +2,10 @@ package clients
 
 import (
 	"context"
-	"github.com/giantswarm/app/v4/pkg/key"
 	"time"
 
 	"github.com/giantswarm/apiextensions/v3/pkg/apis/application/v1alpha1"
+	"github.com/giantswarm/app/v4/pkg/key"
 	"github.com/giantswarm/errors/tenant"
 	"github.com/giantswarm/helmclient/v4/pkg/helmclient"
 	"github.com/giantswarm/k8sclient/v5/pkg/k8sclient"
