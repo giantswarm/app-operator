c47d0ec3a361350d2ffec4a7bb2ce5b972729b96
Ross Fairbanks <ross@rossfairbanks.com>
2021-01-14 10:41:05 +0100

    Remove cluster DNS IP

--- integration/templates/chart_operator_values.go
+++ integration/templates/chart_operator_values.go
@@ -3,8 +3,7 @@
 package templates
 
 // ChartOperatorValues values required by chart-operator-chart.
-const ChartOperatorValues = `clusterDNSIP: 10.96.0.10
-e2e: true
+const ChartOperatorValues = `e2e: true
 
 Installation:
   V1:
