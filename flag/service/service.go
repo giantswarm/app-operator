f26131043cd2be89d278461fb7f8520e8c69a6fa
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2021-02-02 10:54:29 +0100

    Limit the number of app catalog entries (#656)
    * delete appCatalogEntries if desired CRs have none of them
    
    * limit the number of entries
    
    * WIP
    
    * add flag files
    
    * using the latest prometheus operator in the integration test
    
    * fix integration test
    
    * using the older version
    
    * fix entry name
    
    * keep the number deleted CRs in logs and comments
    
    * use appcatalog to fetch the latest entry
    
    * using the latest appcatalog
    
    * Revert "using the latest appcatalog"
    
    This reverts commit 9ba9e16947dd0ff1a445b4a2ea0150f1ec97e1fa.
    
    * Revert "use appcatalog to fetch the latest entry"
    
    This reverts commit 5910a99843b2974b3ab8c8a42e0aabf414976086.
    
    * update go mod
    
    * update appCatalogEntry test
    
    * dynamic version name
    
    * address reviews
    
    * go mod tidy
    
    * CHANGELOG.md

--- flag/service/service.go
+++ flag/service/service.go
@@ -4,6 +4,7 @@ import (
 	"github.com/giantswarm/operatorkit/v4/pkg/flag/service/kubernetes"
 
 	"github.com/giantswarm/app-operator/v3/flag/service/app"
+	"github.com/giantswarm/app-operator/v3/flag/service/appcatalog"
 	"github.com/giantswarm/app-operator/v3/flag/service/chart"
 	"github.com/giantswarm/app-operator/v3/flag/service/helm"
 	"github.com/giantswarm/app-operator/v3/flag/service/image"
@@ -13,6 +14,7 @@ import (
 // Service is an intermediate data structure for command line configuration flags.
 type Service struct {
 	App         app.App
+	AppCatalog  appcatalog.AppCatalog
 	Chart       chart.Chart
 	Helm        helm.Helm
 	Image       image.Image
