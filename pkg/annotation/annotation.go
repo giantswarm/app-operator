42fc06cd9ba6e4438f78c4f8c2653ded624bfda9
Jihyuk Bok <jhbok@giantswarm.io>
2021-02-02 18:09:38 +0100

    review address

--- pkg/annotation/annotation.go
+++ pkg/annotation/annotation.go
@@ -1,6 +1,6 @@
 package annotation
 
 const (
-	// Metadata annotation is storing an app metadata URL from appCatalog's index.yaml.
+	// Metadata annotation stores an app metadata URL from the appCatalog's index.yaml.
 	Metadata = "application.giantswarm.io/metadata"
 )
