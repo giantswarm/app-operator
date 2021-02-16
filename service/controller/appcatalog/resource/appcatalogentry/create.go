a1147ce55528ec73d7d3a6748681230380f09c59
Jihyuk Bok <jhbok@giantswarm.io>
2021-02-16 16:43:05 +0100

    refactor to get latest always

--- service/controller/appcatalog/resource/appcatalogentry/create.go
+++ service/controller/appcatalog/resource/appcatalogentry/create.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"sort"
-	"strconv"
 
 	"github.com/Masterminds/semver/v3"
 	"github.com/giantswarm/apiextensions/v3/pkg/apis/application/v1alpha1"
@@ -170,129 +169,99 @@ func (r *Resource) updateAppCatalogEntry(ctx context.Context, entryCR *v1alpha1.
 	return nil
 }
 
-func (r *Resource) newAppCatalogEntries(ctx context.Context, cr v1alpha1.AppCatalog, index index) (map[string]*v1alpha1.AppCatalogEntry, error) {
-	entryCRs := map[string]*v1alpha1.AppCatalogEntry{}
+func (r *Resource) getDesiredAppCatalogEntryCR(ctx context.Context, cr *v1alpha1.AppCatalog, e entry) (*v1alpha1.AppCatalogEntry, error) {
+	var err error
+	name := fmt.Sprintf("%s-%s-%s", cr.Name, e.Name, e.Version)
 
-	for _, entries := range index.Entries {
-		sort.Slice(entries, func(i, j int) bool {
-			return entries[i].Created.After(entries[j].Created.Time)
-		})
-
-		latestVersion, err := r.getLatestVersion(ctx, entries)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-
-		maxEntries := r.maxEntriesPerApp
-		if len(entries) < maxEntries {
-			maxEntries = len(entries)
-		}
-
-		for i := 0; i < maxEntries; i++ {
-			e := entries[i]
-			name := fmt.Sprintf("%s-%s-%s", cr.Name, e.Name, e.Version)
-
-			var rawMetadata []byte
-			{
-				if url, ok := e.Annotations[annotation.Metadata]; ok {
-					rawMetadata, err = r.getMetadata(ctx, url)
-					if err != nil {
-						r.logger.LogCtx(ctx, "level", "info", "message", fmt.Sprintf("failed to get appMetadata for entry %#q in catalog %#q", e.Name, cr.Name), "stack", fmt.Sprintf("%#v", err))
-						continue
-					}
-				}
+	var rawMetadata []byte
+	{
+		if url, ok := e.Annotations[annotation.Metadata]; ok {
+			rawMetadata, err = r.getMetadata(ctx, url)
+			if err != nil {
+				r.logger.LogCtx(ctx, "level", "info", "message", fmt.Sprintf("failed to get appMetadata for entry %#q in catalog %#q", e.Name, cr.Name), "stack", fmt.Sprintf("%#v", err))
 			}
-
-			// Until we add support for appMetadata files the updated time will be
-			// the same as the created time.
-			updatedTime := e.Created.DeepCopy()
-
-			var m *appMetadata
-			{
-				if rawMetadata != nil {
-					m, err = parseMetadata(rawMetadata)
-					if err != nil {
-						return nil, microerror.Mask(err)
-					}
-				}
-			}
-
-			var isLatest bool
-
-			// We set the latest label to true for easier filtering.
-			if e.Version == latestVersion {
-				isLatest = true
-			}
-
-			entryCR := &v1alpha1.AppCatalogEntry{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: apiVersion,
-					Kind:       kindAppCatalogEntry,
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      name,
-					Namespace: metav1.NamespaceDefault,
-					Labels: map[string]string{
-						label.AppKubernetesName:    e.Name,
-						label.AppKubernetesVersion: e.Version,
-						label.CatalogName:          cr.Name,
-						label.CatalogType:          key.AppCatalogType(cr),
-						pkglabel.Latest:            strconv.FormatBool(isLatest),
-						label.ManagedBy:            key.AppCatalogEntryManagedBy(project.Name()),
-					},
-					OwnerReferences: []metav1.OwnerReference{
-						{
-							APIVersion:         apiVersion,
-							BlockOwnerDeletion: to.BoolP(true),
-							Kind:               kindAppCatalog,
-							Name:               cr.Name,
-							UID:                cr.UID,
-						},
-					},
-				},
-				Spec: v1alpha1.AppCatalogEntrySpec{
-					AppName:    e.Name,
-					AppVersion: e.AppVersion,
-					Catalog: v1alpha1.AppCatalogEntrySpecCatalog{
-						Name: cr.Name,
-						// Namespace will be empty until appcatalog CRs become namespace scoped.
-						Namespace: "",
-					},
-					Chart: v1alpha1.AppCatalogEntrySpecChart{
-						Home: e.Home,
-						Icon: e.Icon,
-					},
-					Version: e.Version,
-				},
-			}
-
-			if m != nil {
-				entryCR.Annotations = m.Annotations
-				entryCR.Spec.Chart.APIVersion = m.ChartAPIVersion
-				entryCR.Spec.Restrictions = m.Restrictions
-				entryCR.Spec.DateCreated = m.DataCreated
-				entryCR.Spec.DateUpdated = m.DataCreated
-			}
-
-			if entryCR.Spec.Chart.APIVersion == "" {
-				// chartAPIVersion default is `v1`.
-				entryCR.Spec.Chart.APIVersion = "v1"
-			}
-
-			if entryCR.Spec.DateCreated == nil {
-				// If meta.yaml does not have dateCreated, use the timestamp from app.
-				entryCR.Spec.DateCreated = &e.Created
-				entryCR.Spec.DateUpdated = updatedTime
-			}
-
-			entryCRs[name] = entryCR
 		}
 	}
 
-	return entryCRs, nil
+	// Until we add support for appMetadata files the updated time will be
+	// the same as the created time.
+	updatedTime := e.Created.DeepCopy()
+
+	var m *appMetadata
+	{
+		if rawMetadata != nil {
+			m, err = parseMetadata(rawMetadata)
+			if err != nil {
+				return nil, microerror.Mask(err)
+			}
+		}
+	}
+
+	entryCR := &v1alpha1.AppCatalogEntry{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: apiVersion,
+			Kind:       kindAppCatalogEntry,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: metav1.NamespaceDefault,
+			Labels: map[string]string{
+				label.AppKubernetesName:    e.Name,
+				label.AppKubernetesVersion: e.Version,
+				label.CatalogName:          cr.Name,
+				label.CatalogType:          key.AppCatalogType(*cr),
+				label.ManagedBy:            key.AppCatalogEntryManagedBy(project.Name()),
+			},
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					APIVersion:         apiVersion,
+					BlockOwnerDeletion: to.BoolP(true),
+					Kind:               kindAppCatalog,
+					Name:               cr.Name,
+					UID:                cr.UID,
+				},
+			},
+		},
+		Spec: v1alpha1.AppCatalogEntrySpec{
+			AppName:    e.Name,
+			AppVersion: e.AppVersion,
+			Catalog: v1alpha1.AppCatalogEntrySpecCatalog{
+				Name: cr.Name,
+				// Namespace will be empty until appcatalog CRs become namespace scoped.
+				Namespace: "",
+			},
+			Chart: v1alpha1.AppCatalogEntrySpecChart{
+				Home: e.Home,
+				Icon: e.Icon,
+			},
+			Version: e.Version,
+		},
+	}
+
+	if m != nil {
+		entryCR.Annotations = m.Annotations
+		entryCR.Spec.Chart.APIVersion = m.ChartAPIVersion
+		entryCR.Spec.Restrictions = m.Restrictions
+		entryCR.Spec.DateCreated = m.DataCreated
+		entryCR.Spec.DateUpdated = m.DataCreated
+	}
+
+	if entryCR.Spec.Chart.APIVersion == "" {
+		// chartAPIVersion default is `v1`.
+		entryCR.Spec.Chart.APIVersion = "v1"
+	}
+
+	if entryCR.Spec.DateCreated == nil {
+		// If meta.yaml does not have dateCreated, use the timestamp from app.
+		entryCR.Spec.DateCreated = &e.Created
+		entryCR.Spec.DateUpdated = updatedTime
+	}
+
+	return entryCR, nil
 }
 
-func (r *Resource) getLatestVersion(ctx context.Context, entries []entry) (string, error) {
+func (r *Resource) getLatestEntry(ctx context.Context, entries []entry) (entry, error) {
+	var latestIndex int
 	var latestVersion semver.Version
 	var latestCreated *metav1.Time
 
@@ -302,16 +271,17 @@ func (r *Resource) getLatestVersion(ctx context.Context, entries []entry) (strin
 			r.logger.Debugf(ctx, "invalid semver from converting app entries %s, version is %s", entries[i].Name, entries[i].Version)
 			continue
 		} else if err != nil {
-			return "", microerror.Mask(err)
+			return entry{}, microerror.Mask(err)
 		}
 
 		// Removing Prerelease from version since they are mostly SHA strings which we could not compare the size.
 		nextVersion, err := v.SetPrerelease("")
 		if err != nil {
-			return "", microerror.Mask(err)
+			return entry{}, microerror.Mask(err)
 		}
 
 		if nextVersion.GreaterThan(&latestVersion) {
+			latestIndex = i
 			latestVersion = nextVersion
 			latestCreated = entries[i].Created.DeepCopy()
 			continue
@@ -319,11 +289,57 @@ func (r *Resource) getLatestVersion(ctx context.Context, entries []entry) (strin
 
 		if nextVersion.Equal(&latestVersion) {
 			if latestCreated != nil && latestCreated.After(entries[i].Created.Time) {
+				latestIndex = i
 				latestVersion = nextVersion
 				latestCreated = entries[i].Created.DeepCopy()
 			}
 		}
 	}
 
-	return latestVersion.String(), nil
+	return entries[latestIndex], nil
+}
+
+func (r *Resource) newAppCatalogEntries(ctx context.Context, cr v1alpha1.AppCatalog, index index) (map[string]*v1alpha1.AppCatalogEntry, error) {
+	entryCRs := map[string]*v1alpha1.AppCatalogEntry{}
+
+	for _, entries := range index.Entries {
+		sort.Slice(entries, func(i, j int) bool {
+			return entries[i].Created.After(entries[j].Created.Time)
+		})
+
+		maxEntries := r.maxEntriesPerApp
+		if len(entries) < maxEntries {
+			maxEntries = len(entries)
+		}
+
+		for i := 0; i < maxEntries; i++ {
+			e := entries[i]
+
+			entryCR, err := r.getDesiredAppCatalogEntryCR(ctx, &cr, e)
+			if err != nil {
+				return nil, microerror.Mask(err)
+			}
+
+			entryCRs[entryCR.Name] = entryCR
+		}
+
+		latestEntry, err := r.getLatestEntry(ctx, entries)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+
+		if v, ok := entryCRs[latestEntry.Name]; !ok {
+			entryCR, err := r.getDesiredAppCatalogEntryCR(ctx, &cr, latestEntry)
+			if err != nil {
+				return nil, microerror.Mask(err)
+			}
+
+			entryCR.Labels[pkglabel.Latest] = "true"
+			entryCRs[latestEntry.Name] = entryCR
+		} else {
+			v.Labels[pkglabel.Latest] = "true"
+		}
+	}
+
+	return entryCRs, nil
 }
