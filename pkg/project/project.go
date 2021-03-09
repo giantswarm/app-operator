acd0c53485790384ba81737e09f2bc0f29e60c98
GitHub Action <action@github.com>
2021-03-09 13:09:04 +0000

    Bump version to 4.0.3-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-operator"
 	source      = "https://github.com/giantswarm/app-operator"
-	version     = "4.0.2"
+	version     = "4.0.3-dev"
 )
 
 func Description() string {
