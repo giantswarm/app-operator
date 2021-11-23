351b6d34d0f109c54c654700d290667556dfe0d7
Ross Fairbanks <ross@rossfairbanks.com>
2021-11-23 17:57:48 +0100

    More fixes and use config branch

--- integration/templates/app_operator_values.go
+++ integration/templates/app_operator_values.go
@@ -1,11 +1,10 @@
+//go:build k8srequired
 // +build k8srequired
 
 package templates
 
 // AppOperatorValues values required by app-operator-chart.
 const AppOperatorValues = `
-Installation:
-  V1:
-    Registry:
-      Domain: quay.io
+  registry:
+    domain: quay.io
 `
