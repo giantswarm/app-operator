cf865e9444fa482fb5edd65466d71514404c49e0
Ross Fairbanks <ross@rossfairbanks.com>
2021-11-24 09:28:03 +0100

    Add missing values for integration test

--- integration/templates/app_operator_values.go
+++ integration/templates/app_operator_values.go
@@ -5,6 +5,12 @@ package templates
 
 // AppOperatorValues values required by app-operator-chart.
 const AppOperatorValues = `
+  chart:
+    webhook:
+      authToken: auth-token
+      baseURL: g8s.gauss.eu-west-1.aws.gigantic.io
+  provider:
+    kind: aws
   registry:
     domain: quay.io
 `
