df561f093908489db23df6d19ae60d95f89f5077
Ross Fairbanks <ross@rossfairbanks.com>
2021-12-15 11:00:56 +0100

    Change branch

--- integration/setup/setup.sh
+++ integration/setup/setup.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-curl -L https://github.com/giantswarm/apptestctl/releases/download/v0.12.0/apptestctl-v0.12.0-linux-amd64.tar.gz > ./apptestctl.tar.gz
-tar xzvf apptestctl.tar.gz
-chmod u+x apptestctl-v0.12.0-linux-amd64/apptestctl
-sudo mv apptestctl-v0.12.0-linux-amd64/apptestctl /usr/local/bin
-
-apptestctl bootstrap --kubeconfig="$(kind get kubeconfig)" --install-operators=false --wait=false
+apptestctl bootstrap --kubeconfig="$(kind get kubeconfig)" --install-operators=false
