1223a74e4fd3931ecdd35ddb9b71a5196481688e
Ross Fairbanks <rossf7@users.noreply.github.com>
2022-02-01 19:12:05 +0100

    Revert "Disable unit tests"
    This reverts commit 9585d6652ba275dede1ec52fc3bad8b43ac9f178.

--- service/controller/app/resource/chart/update_test.go
+++ service/controller/app/resource/chart/update_test.go
@@ -1,239 +1,249 @@
 package chart
 
 import (
+	"context"
+	"reflect"
 	"testing"
-	//nolint:staticcheck
+
+	"github.com/giantswarm/apiextensions-application/api/v1alpha1"
+	"github.com/giantswarm/k8sclient/v6/pkg/k8sclienttest"
+	"github.com/giantswarm/micrologger/microloggertest"
+	"github.com/google/go-cmp/cmp"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgofake "k8s.io/client-go/kubernetes/fake"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake" //nolint:staticcheck
+
+	"github.com/giantswarm/app-operator/v5/service/controller/app/controllercontext"
 )
 
 func Test_Resource_newUpdateChange(t *testing.T) {
-	/*
-		tests := []struct {
-			name          string
-			currentChart  *v1alpha1.Chart
-			desiredChart  *v1alpha1.Chart
-			expectedChart *v1alpha1.Chart
-			error         bool
-		}{
-			{
-				name: "case 0: flawless flow",
-				currentChart: &v1alpha1.Chart{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "Chart",
-						APIVersion: "application.giantswarm.io",
+	tests := []struct {
+		name          string
+		currentChart  *v1alpha1.Chart
+		desiredChart  *v1alpha1.Chart
+		expectedChart *v1alpha1.Chart
+		error         bool
+	}{
+		{
+			name: "case 0: flawless flow",
+			currentChart: &v1alpha1.Chart{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Chart",
+					APIVersion: "application.giantswarm.io",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:            "my-cool-prometheus",
+					Namespace:       "giantswarm",
+					ResourceVersion: "12345",
+					UID:             "51eeec1d-3716-4006-92b4-e7e99f8ab311",
+					Annotations: map[string]string{
+						"chart-operator.giantswarm.io/values-md5-checksum": "1678b4446ba0392da6681840add3d06a",
+						"giantswarm.io/sample":                             "it should be deleted",
 					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name:            "my-cool-prometheus",
-						Namespace:       "giantswarm",
-						ResourceVersion: "12345",
-						UID:             "51eeec1d-3716-4006-92b4-e7e99f8ab311",
-						Annotations: map[string]string{
-							"chart-operator.giantswarm.io/values-md5-checksum": "1678b4446ba0392da6681840add3d06a",
-							"giantswarm.io/sample":                             "it should be deleted",
-						},
-						Labels: map[string]string{
-							"giantswarm.io/managed-by": "app-operator",
-						},
-					},
-					Spec: v1alpha1.ChartSpec{
-						Config: v1alpha1.ChartSpecConfig{
-							ConfigMap: v1alpha1.ChartSpecConfigConfigMap{
-								Name:      "my-cool-prometheus-chart-values",
-								Namespace: "giantswarm",
-							},
-						},
-						Name:       "my-cool-prometheus",
-						Namespace:  "monitoring",
-						TarballURL: "https://giantswarm.github.io/app-catalog/prometheus-1.0.0.tgz",
-						Version:    "0.0.9",
+					Labels: map[string]string{
+						"giantswarm.io/managed-by": "app-operator",
 					},
 				},
-				desiredChart: &v1alpha1.Chart{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "Chart",
-						APIVersion: "application.giantswarm.io",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "my-cool-prometheus",
-						Namespace: "giantswarm",
-						Labels: map[string]string{
-							"app":                                  "prometheus",
-							"chart-operator.giantswarm.io/version": "1.0.0",
-							"giantswarm.io/managed-by":             "app-operator",
-						},
-						Annotations: map[string]string{
-							"chart-operator.giantswarm.io/webhook-url": "http://webhook/status/default/my-cool-prometheus",
+				Spec: v1alpha1.ChartSpec{
+					Config: v1alpha1.ChartSpecConfig{
+						ConfigMap: v1alpha1.ChartSpecConfigConfigMap{
+							Name:      "my-cool-prometheus-chart-values",
+							Namespace: "giantswarm",
 						},
 					},
-					Spec: v1alpha1.ChartSpec{
-						Config: v1alpha1.ChartSpecConfig{
-							ConfigMap: v1alpha1.ChartSpecConfigConfigMap{
-								Name:      "my-cool-prometheus-chart-values",
-								Namespace: "giantswarm",
-							},
-						},
-						Name:       "my-cool-prometheus",
-						Namespace:  "monitoring",
-						TarballURL: "https://giantswarm.github.io/app-catalog/prometheus-1.0.0.tgz",
-						Version:    "1.0.0",
-					},
-				},
-				expectedChart: &v1alpha1.Chart{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "Chart",
-						APIVersion: "application.giantswarm.io",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name:            "my-cool-prometheus",
-						Namespace:       "giantswarm",
-						ResourceVersion: "12345",
-						Labels: map[string]string{
-							"app":                                  "prometheus",
-							"chart-operator.giantswarm.io/version": "1.0.0",
-							"giantswarm.io/managed-by":             "app-operator",
-						},
-						Annotations: map[string]string{
-							"chart-operator.giantswarm.io/values-md5-checksum": "1678b4446ba0392da6681840add3d06a",
-							"chart-operator.giantswarm.io/webhook-url":         "http://webhook/status/default/my-cool-prometheus",
-						},
-					},
-					Spec: v1alpha1.ChartSpec{
-						Config: v1alpha1.ChartSpecConfig{
-							ConfigMap: v1alpha1.ChartSpecConfigConfigMap{
-								Name:      "my-cool-prometheus-chart-values",
-								Namespace: "giantswarm",
-							},
-						},
-						Name:       "my-cool-prometheus",
-						Namespace:  "monitoring",
-						TarballURL: "https://giantswarm.github.io/app-catalog/prometheus-1.0.0.tgz",
-						Version:    "1.0.0",
-					},
+					Name:       "my-cool-prometheus",
+					Namespace:  "monitoring",
+					TarballURL: "https://giantswarm.github.io/app-catalog/prometheus-1.0.0.tgz",
+					Version:    "0.0.9",
 				},
 			},
-			{
-				name: "case 1: same chart",
-				currentChart: &v1alpha1.Chart{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "Chart",
-						APIVersion: "application.giantswarm.io",
+			desiredChart: &v1alpha1.Chart{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Chart",
+					APIVersion: "application.giantswarm.io",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "my-cool-prometheus",
+					Namespace: "giantswarm",
+					Labels: map[string]string{
+						"app":                                  "prometheus",
+						"chart-operator.giantswarm.io/version": "1.0.0",
+						"giantswarm.io/managed-by":             "app-operator",
 					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "my-cool-prometheus",
-						Namespace: "giantswarm",
-						Labels: map[string]string{
-							"app":                                  "prometheus",
-							"chart-operator.giantswarm.io/version": "1.0.0",
-							"giantswarm.io/managed-by":             "app-operator",
-						},
-						Annotations: map[string]string{
-							"chart-operator.giantswarm.io/values-md5-checksum": "1678b4446ba0392da6681840add3d06a",
-							"chart-operator.giantswarm.io/webhook-url":         "http://webhook/status/default/my-cool-prometheus",
-						},
-					},
-					Spec: v1alpha1.ChartSpec{
-						Config: v1alpha1.ChartSpecConfig{
-							ConfigMap: v1alpha1.ChartSpecConfigConfigMap{
-								Name:      "my-cool-prometheus-chart-values",
-								Namespace: "giantswarm",
-							},
-						},
-						Name:       "my-cool-prometheus",
-						Namespace:  "monitoring",
-						TarballURL: "https://giantswarm.github.io/app-catalog/prometheus-1.0.0.tgz",
-						Version:    "1.0.0",
+					Annotations: map[string]string{
+						"chart-operator.giantswarm.io/webhook-url": "http://webhook/status/default/my-cool-prometheus",
 					},
 				},
-				desiredChart: &v1alpha1.Chart{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "Chart",
-						APIVersion: "application.giantswarm.io",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "my-cool-prometheus",
-						Namespace: "giantswarm",
-						Labels: map[string]string{
-							"app":                                  "prometheus",
-							"chart-operator.giantswarm.io/version": "1.0.0",
-							"giantswarm.io/managed-by":             "app-operator",
-						},
-						Annotations: map[string]string{
-							"chart-operator.giantswarm.io/webhook-url": "http://webhook/status/default/my-cool-prometheus",
+				Spec: v1alpha1.ChartSpec{
+					Config: v1alpha1.ChartSpecConfig{
+						ConfigMap: v1alpha1.ChartSpecConfigConfigMap{
+							Name:      "my-cool-prometheus-chart-values",
+							Namespace: "giantswarm",
 						},
 					},
-					Spec: v1alpha1.ChartSpec{
-						Config: v1alpha1.ChartSpecConfig{
-							ConfigMap: v1alpha1.ChartSpecConfigConfigMap{
-								Name:      "my-cool-prometheus-chart-values",
-								Namespace: "giantswarm",
-							},
-						},
-						Name:       "my-cool-prometheus",
-						Namespace:  "monitoring",
-						TarballURL: "https://giantswarm.github.io/app-catalog/prometheus-1.0.0.tgz",
-						Version:    "1.0.0",
-					},
+					Name:       "my-cool-prometheus",
+					Namespace:  "monitoring",
+					TarballURL: "https://giantswarm.github.io/app-catalog/prometheus-1.0.0.tgz",
+					Version:    "1.0.0",
 				},
-				expectedChart: &v1alpha1.Chart{},
 			},
-		}
+			expectedChart: &v1alpha1.Chart{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Chart",
+					APIVersion: "application.giantswarm.io",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:            "my-cool-prometheus",
+					Namespace:       "giantswarm",
+					ResourceVersion: "12345",
+					Labels: map[string]string{
+						"app":                                  "prometheus",
+						"chart-operator.giantswarm.io/version": "1.0.0",
+						"giantswarm.io/managed-by":             "app-operator",
+					},
+					Annotations: map[string]string{
+						"chart-operator.giantswarm.io/values-md5-checksum": "1678b4446ba0392da6681840add3d06a",
+						"chart-operator.giantswarm.io/webhook-url":         "http://webhook/status/default/my-cool-prometheus",
+					},
+				},
+				Spec: v1alpha1.ChartSpec{
+					Config: v1alpha1.ChartSpecConfig{
+						ConfigMap: v1alpha1.ChartSpecConfigConfigMap{
+							Name:      "my-cool-prometheus-chart-values",
+							Namespace: "giantswarm",
+						},
+					},
+					Name:       "my-cool-prometheus",
+					Namespace:  "monitoring",
+					TarballURL: "https://giantswarm.github.io/app-catalog/prometheus-1.0.0.tgz",
+					Version:    "1.0.0",
+				},
+			},
+		},
+		{
+			name: "case 1: same chart",
+			currentChart: &v1alpha1.Chart{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Chart",
+					APIVersion: "application.giantswarm.io",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "my-cool-prometheus",
+					Namespace: "giantswarm",
+					Labels: map[string]string{
+						"app":                                  "prometheus",
+						"chart-operator.giantswarm.io/version": "1.0.0",
+						"giantswarm.io/managed-by":             "app-operator",
+					},
+					Annotations: map[string]string{
+						"chart-operator.giantswarm.io/values-md5-checksum": "1678b4446ba0392da6681840add3d06a",
+						"chart-operator.giantswarm.io/webhook-url":         "http://webhook/status/default/my-cool-prometheus",
+					},
+				},
+				Spec: v1alpha1.ChartSpec{
+					Config: v1alpha1.ChartSpecConfig{
+						ConfigMap: v1alpha1.ChartSpecConfigConfigMap{
+							Name:      "my-cool-prometheus-chart-values",
+							Namespace: "giantswarm",
+						},
+					},
+					Name:       "my-cool-prometheus",
+					Namespace:  "monitoring",
+					TarballURL: "https://giantswarm.github.io/app-catalog/prometheus-1.0.0.tgz",
+					Version:    "1.0.0",
+				},
+			},
+			desiredChart: &v1alpha1.Chart{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Chart",
+					APIVersion: "application.giantswarm.io",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "my-cool-prometheus",
+					Namespace: "giantswarm",
+					Labels: map[string]string{
+						"app":                                  "prometheus",
+						"chart-operator.giantswarm.io/version": "1.0.0",
+						"giantswarm.io/managed-by":             "app-operator",
+					},
+					Annotations: map[string]string{
+						"chart-operator.giantswarm.io/webhook-url": "http://webhook/status/default/my-cool-prometheus",
+					},
+				},
+				Spec: v1alpha1.ChartSpec{
+					Config: v1alpha1.ChartSpecConfig{
+						ConfigMap: v1alpha1.ChartSpecConfigConfigMap{
+							Name:      "my-cool-prometheus-chart-values",
+							Namespace: "giantswarm",
+						},
+					},
+					Name:       "my-cool-prometheus",
+					Namespace:  "monitoring",
+					TarballURL: "https://giantswarm.github.io/app-catalog/prometheus-1.0.0.tgz",
+					Version:    "1.0.0",
+				},
+			},
+			expectedChart: &v1alpha1.Chart{},
+		},
+	}
 
-		for _, tc := range tests {
-			t.Run(tc.name, func(t *testing.T) {
-				objs := make([]runtime.Object, 0)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			objs := make([]runtime.Object, 0)
 
-				c := Config{
-					Logger: microloggertest.New(),
+			c := Config{
+				Logger: microloggertest.New(),
 
-					ChartNamespace: "giantswarm",
+				ChartNamespace: "giantswarm",
+			}
+			r, err := New(c)
+			if err != nil {
+				t.Fatalf("error == %#v, want nil", err)
+			}
+
+			var ctx context.Context
+			{
+				config := k8sclienttest.ClientsConfig{
+					CtrlClient: fake.NewFakeClient(), //nolint:staticcheck
+					K8sClient:  clientgofake.NewSimpleClientset(objs...),
 				}
-				r, err := New(c)
+				client := k8sclienttest.NewClients(config)
+
+				c := controllercontext.Context{
+					Clients: controllercontext.Clients{
+						K8s: client,
+					},
+				}
+				ctx = controllercontext.NewContext(context.Background(), c)
+			}
+
+			result, err := r.newUpdateChange(ctx, tc.currentChart, tc.desiredChart)
+			switch {
+			case err != nil && !tc.error:
+				t.Fatalf("error == %#v, want nil", err)
+			case err == nil && tc.error:
+				t.Fatalf("error == nil, want non-nil")
+			}
+
+			if err == nil && !tc.error {
+				chart, err := toChart(result)
 				if err != nil {
 					t.Fatalf("error == %#v, want nil", err)
 				}
 
-				var ctx context.Context
-				{
-					config := k8sclienttest.ClientsConfig{
-						CtrlClient: fake.NewFakeClient(), //nolint:staticcheck
-						K8sClient:  clientgofake.NewSimpleClientset(objs...),
-					}
-					client := k8sclienttest.NewClients(config)
-
-					c := controllercontext.Context{
-						Clients: controllercontext.Clients{
-							K8s: client,
-						},
-					}
-					ctx = controllercontext.NewContext(context.Background(), c)
+				if !reflect.DeepEqual(chart.ObjectMeta, tc.expectedChart.ObjectMeta) {
+					t.Fatalf("want matching objectmeta \n %s", cmp.Diff(chart.ObjectMeta, tc.expectedChart.ObjectMeta))
 				}
 
-				result, err := r.newUpdateChange(ctx, tc.currentChart, tc.desiredChart)
-				switch {
-				case err != nil && !tc.error:
-					t.Fatalf("error == %#v, want nil", err)
-				case err == nil && tc.error:
-					t.Fatalf("error == nil, want non-nil")
+				if !reflect.DeepEqual(chart.Spec, tc.expectedChart.Spec) {
+					t.Fatalf("want matching spec \n %s", cmp.Diff(chart.Spec, tc.expectedChart.Spec))
 				}
 
-				if err == nil && !tc.error {
-					chart, err := toChart(result)
-					if err != nil {
-						t.Fatalf("error == %#v, want nil", err)
-					}
-
-					if !reflect.DeepEqual(chart.ObjectMeta, tc.expectedChart.ObjectMeta) {
-						t.Fatalf("want matching objectmeta \n %s", cmp.Diff(chart.ObjectMeta, tc.expectedChart.ObjectMeta))
-					}
-
-					if !reflect.DeepEqual(chart.Spec, tc.expectedChart.Spec) {
-						t.Fatalf("want matching spec \n %s", cmp.Diff(chart.Spec, tc.expectedChart.Spec))
-					}
-
-					if !reflect.DeepEqual(chart.TypeMeta, tc.expectedChart.TypeMeta) {
-						t.Fatalf("want matching typemeta \n %s", cmp.Diff(chart.TypeMeta, tc.expectedChart.TypeMeta))
-					}
+				if !reflect.DeepEqual(chart.TypeMeta, tc.expectedChart.TypeMeta) {
+					t.Fatalf("want matching typemeta \n %s", cmp.Diff(chart.TypeMeta, tc.expectedChart.TypeMeta))
 				}
-			})
-		}
-	*/
+			}
+		})
+	}
 }
