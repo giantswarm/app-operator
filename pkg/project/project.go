8c62727df15cae59cf55f0537f8aa8a1dfcf876f
GitHub Action <action@github.com>
2022-05-18 10:09:37 +0000

    Bump version to 5.10.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-operator"
 	source      = "https://github.com/giantswarm/app-operator"
-	version     = "5.10.1"
+	version     = "5.10.2-dev"
 )
 
 func Description() string {
