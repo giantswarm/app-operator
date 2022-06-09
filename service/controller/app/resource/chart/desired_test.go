1d47bd9f1b403aaddca2a4a38c3b97887f061054
kubasobon <wtty.fool@gmail.com>
2022-06-09 09:25:05 +0200

    prepare new tests

--- service/controller/app/resource/chart/desired_test.go
+++ service/controller/app/resource/chart/desired_test.go
@@ -526,6 +526,175 @@ func Test_Resource_GetDesiredState(t *testing.T) {
 	}
 }
 
+func Test_Resource_Bulid_TarballURL(t *testing.T) {
+	app := &v1alpha1.App{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-cool-prometheus",
+			Namespace: "default",
+			Labels: map[string]string{
+				"app":                                "prometheus",
+				"app-operator.giantswarm.io/version": "1.0.0",
+				"giantswarm.io/managed-by":           "cluster-operator",
+			},
+		},
+		Spec: v1alpha1.AppSpec{
+			Catalog:   "giantswarm",
+			Name:      "prometheus",
+			Namespace: "monitoring",
+			NamespaceConfig: v1alpha1.AppSpecNamespaceConfig{
+				Annotations: map[string]string{
+					"linkerd.io/inject": "enabled",
+				},
+			},
+			Version: "1.0.0",
+			Config:  v1alpha1.AppSpecConfig{},
+			KubeConfig: v1alpha1.AppSpecKubeConfig{
+				Secret: v1alpha1.AppSpecKubeConfigSecret{
+					Name:      "giantswarm-12345",
+					Namespace: "12345",
+				},
+			},
+		},
+	}
+
+	tests := []struct {
+		name          string
+		obj           *v1alpha1.App
+		catalog       v1alpha1.Catalog
+		index         *indexcache.Index
+		expectedChart *v1alpha1.Chart
+		errorPattern  *regexp.Regexp
+		error         bool
+	}{
+		{
+			name: "case 0: flawless flow",
+			obj:  app,
+			catalog: v1alpha1.Catalog{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "giantswarm",
+					Namespace: "default",
+					Labels: map[string]string{
+						"app-operator.giantswarm.io/version": "1.0.0",
+					},
+				},
+				Spec: v1alpha1.CatalogSpec{
+					Title:       "Giant Swarm",
+					Description: "Catalog of Apps by Giant Swarm",
+					Storage: v1alpha1.CatalogSpecStorage{
+						Type: "helm",
+						URL:  "https://giantswarm.github.io/app-catalog/",
+					},
+					Repositories: []v1alpha1.CatalogSpecRepository{
+						{
+							Type: "helm",
+							URL:  "https://giantswarm.github.io/app-catalog/",
+						},
+					},
+					LogoURL: "https://s.giantswarm.io/...",
+				},
+			},
+			index: newIndexWithApp("prometheus", "1.0.0", "https://giantswarm.github.io/app-catalog/prometheus-1.0.0.tgz"),
+			expectedChart: &v1alpha1.Chart{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Chart",
+					APIVersion: "application.giantswarm.io",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "my-cool-prometheus",
+					Namespace: "giantswarm",
+					Annotations: map[string]string{
+						"chart-operator.giantswarm.io/app-name":      "my-cool-prometheus",
+						"chart-operator.giantswarm.io/app-namespace": "default",
+					},
+					Labels: map[string]string{
+						"app":                                  "prometheus",
+						"chart-operator.giantswarm.io/version": "1.0.0",
+						"giantswarm.io/managed-by":             "app-operator",
+					},
+				},
+				Spec: v1alpha1.ChartSpec{
+					Config:    v1alpha1.ChartSpecConfig{},
+					Name:      "my-cool-prometheus",
+					Namespace: "monitoring",
+					NamespaceConfig: v1alpha1.ChartSpecNamespaceConfig{
+						Annotations: map[string]string{
+							"linkerd.io/inject": "enabled",
+						},
+					},
+					TarballURL: "https://giantswarm.github.io/app-catalog/prometheus-1.0.0.tgz",
+					Version:    "1.0.0",
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			objs := make([]runtime.Object, 0)
+
+			c := Config{
+				IndexCache: indexcachetest.New(indexcachetest.Config{
+					GetIndexResponse: tc.index,
+				}),
+				Logger: microloggertest.New(),
+
+				ChartNamespace: "giantswarm",
+			}
+			r, err := New(c)
+			if err != nil {
+				t.Fatalf("error == %#v, want nil", err)
+			}
+
+			var ctx context.Context
+			{
+				s := runtime.NewScheme()
+				s.AddKnownTypes(v1alpha1.SchemeGroupVersion, &v1alpha1.Chart{}, &v1alpha1.ChartList{})
+				config := k8sclienttest.ClientsConfig{
+					CtrlClient: fake.NewFakeClientWithScheme(s), //nolint:staticcheck
+					K8sClient:  clientgofake.NewSimpleClientset(objs...),
+				}
+				client := k8sclienttest.NewClients(config)
+
+				c := controllercontext.Context{
+					Clients: controllercontext.Clients{
+						K8s: client,
+					},
+					Catalog: tc.catalog,
+				}
+				ctx = controllercontext.NewContext(context.Background(), c)
+			}
+
+			result, err := r.GetDesiredState(ctx, tc.obj)
+			switch {
+			case err != nil && !tc.error:
+				t.Fatalf("error == %#v, want nil", err)
+			case err == nil && tc.error:
+				t.Fatalf("error == nil, want non-nil")
+			case err != nil && tc.error && !tc.errorPattern.MatchString(microerror.Pretty(err, true)):
+				t.Fatalf("error == %q does not match expected pattern %q", err.Error(), tc.errorPattern.String())
+			}
+
+			if err == nil && !tc.error {
+				chart, err := toChart(result)
+				if err != nil {
+					t.Fatalf("error == %#v, want nil", err)
+				}
+
+				if !reflect.DeepEqual(chart.ObjectMeta, tc.expectedChart.ObjectMeta) {
+					t.Fatalf("want matching objectmeta \n %s", cmp.Diff(chart.ObjectMeta, tc.expectedChart.ObjectMeta))
+				}
+
+				if !reflect.DeepEqual(chart.Spec, tc.expectedChart.Spec) {
+					t.Fatalf("want matching spec \n %s", cmp.Diff(chart.Spec, tc.expectedChart.Spec))
+				}
+
+				if !reflect.DeepEqual(chart.TypeMeta, tc.expectedChart.TypeMeta) {
+					t.Fatalf("want matching typemeta \n %s", cmp.Diff(chart.TypeMeta, tc.expectedChart.TypeMeta))
+				}
+			}
+		})
+	}
+}
 func Test_generateConfig(t *testing.T) {
 	tests := []struct {
 		name           string
