47711ea7c3c54a307b95db2d3d475acd86234f19
Łukasz Jakimczuk <lukasz.j@giantswarm.io>
2023-02-01 15:02:11 +0100

    Changing api version

--- integration/test/catalog/appcatalogentry/app_catalog_entry_test.go
+++ integration/test/catalog/appcatalogentry/app_catalog_entry_test.go
@@ -138,7 +138,7 @@ func TestAppCatalogEntry(t *testing.T) {
 				Namespace: metav1.NamespaceDefault,
 			},
 			Chart: v1alpha1.AppCatalogEntrySpecChart{
-				APIVersion:  "v1",
+				APIVersion:  "v2",
 				Description: latestEntry.Description,
 				Home:        latestEntry.Home,
 				Icon:        latestEntry.Icon,
