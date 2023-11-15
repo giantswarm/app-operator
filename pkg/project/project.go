0b6c128b8b85faeed17d79c981111c9a175cdc2c
taylorbot <dev@giantswarm.io>
2023-11-15 08:29:28 +0000

    Bump version to 6.10.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-operator"
 	source      = "https://github.com/giantswarm/app-operator"
-	version     = "6.10.0"
+	version     = "6.10.1-dev"
 )
 
 func Description() string {
