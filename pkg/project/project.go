3698d29b948543936c355a954325e4ff0ac247db
taylorbot <dev@giantswarm.io>
2024-04-23 12:18:03 +0000

    Release v6.11.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-operator"
 	source      = "https://github.com/giantswarm/app-operator"
-	version     = "6.10.4-dev"
+	version     = "6.11.0"
 )
 
 func Description() string {
