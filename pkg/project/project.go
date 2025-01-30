688ef8671b8a79962afe8eed8953754320e103f8
taylorbot <dev@giantswarm.io>
2025-01-30 13:10:05 +0000

    Bump version to 7.0.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-operator"
 	source      = "https://github.com/giantswarm/app-operator"
-	version     = "7.0.1"
+	version     = "7.0.2-dev"
 )
 
 func Description() string {
