9e15c76111a37c7d8bd487c2eb5c5cb121281ed7
Łukasz Jakimczuk <lukasz.j@giantswarm.io>
2025-10-21 14:13:37 +0200

    Improving Chart Operator deletion

--- service/controller/app/resource/chartoperator/delete.go
+++ service/controller/app/resource/chartoperator/delete.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/giantswarm/app/v8/pkg/key"
 	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/v7/pkg/controller/context/reconciliationcanceledcontext"
+	"k8s.io/apimachinery/pkg/types"
+	capi "sigs.k8s.io/cluster-api/api/v1beta1"
 
 	"github.com/giantswarm/app-operator/v7/service/controller/app/controllercontext"
 )
@@ -22,9 +25,30 @@ func (r Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 	// Resource is used to bootstrap chart-operator. So for other apps we can
 	// skip this step.
 	if key.AppName(cr) != key.ChartOperatorAppName {
-		r.logger.Debugf(ctx, "no need to install chart-operator for %#q", key.AppName(cr))
-		r.logger.Debugf(ctx, "canceling resource")
 		return nil
+	}
+
+	// Check if cluster is being deleted
+	clusterID := key.ClusterID(cr)
+
+	if clusterID != "" {
+		capiCluster := &capi.Cluster{}
+		err = r.ctrlClient.Get(
+			ctx,
+			types.NamespacedName{Name: clusterID, Namespace: cr.Namespace},
+			capiCluster,
+		)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		if capiCluster.GetDeletionTimestamp() != nil {
+			// Canceling reconciliation stops processing of further
+			// resource and makes the OperatorKit to remove the finalizer.
+			// This is what we want for Chart Operator upon cluster deletion.
+			reconciliationcanceledcontext.SetCanceled(ctx)
+			return nil
+		}
 	}
 
 	if cc.Status.ClusterStatus.IsDeleting {
