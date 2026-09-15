7f52e6904b1c5ef3320f27534f68ce62b2c24708
Łukasz Jakimczuk <lukasz.j@giantswarm.io>
2026-09-15 15:26:16 +0200

    Switching to gitsemver-gen version

--- integration/key/key.go
+++ integration/key/key.go
@@ -21,7 +21,7 @@ func AppOperatorInTestVersion() string {
 	if strings.HasSuffix(project.Version(), "-dev") || !env.IsMainBranch() {
 		// In case of running the tests against a development version, the artifact is uploaded to the test catalog
 		// with the SHA1 postfixed to the version, e.g. app-operator-5.11.0-19b12a1e4e9ea3e9733ae1d3bb6b33830d8c2738.tgz
-		version = env.CircleSHA()
+		version = env.BuildVersion()
 	} else {
 		// In case of running the tests against a release it is only uploaded to the test catalog with the project version,
 		// for example: app-operator-6.0.0.tgz (no SHA1 postfixed version is available)
